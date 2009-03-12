7cb3d899bc9712fcfc9c9ef6a529a08a51d7ba13
sasha <sasha>
2009-03-12 08:27:03 +0000

    ssh wrapper module

---
+++ ssh_utilities.py
@@ -0,0 +1,34 @@
+import pexpect
+from pxssh import pxssh, ExceptionPxssh
+
+def ssh_execute(user, host, password, command):
+    ssh_answer = 'Are you sure you want to continue connecting'
+    ssession = pexpect.spawn('ssh -l %s %s %s'%(user, host, command))
+    reply = ssession.expect([pexpect.TIMEOUT, ssh_answer, 'password: '])
+    if reply == 0: # Timeout
+        return 'ERROR:' + ssession.before + ':' + ssession.after
+    if reply == 1: # SSH does not have the public key. Just accept it.
+        ssession.sendline ('yes')
+        ssession.expect ('password: ')
+        reply = child.expect([pexpect.TIMEOUT, 'password: '])
+        if reply == 0: # Timeout
+            return 'ERROR:' + ssession.before + ':' + ssession.after      
+    ssession.sendline(password)
+    ssession.expect(pexpect.EOF)
+    return 'OK:' + ssession.before + ':' + ssession.after 
+
+class SSHClient(pxssh):
+    def __init__(self, host, port, username, password):
+        pxssh.__init__(self)
+        self.force_password = True
+        self.login(host, username,password=password, port=port)
+
+    def send_command(self, text):
+        self.sendline(text)
+        self.prompt()
+        return self.before
+
+    def close_channel(self):
+        self.logout()
+
+
