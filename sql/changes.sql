75958db614c2a8996c6e9a2beba97b175687c7a2
sasha <sasha>
2009-04-16 07:45:19 +0000

    sql changes

--- sql/changes.sql
+++ sql/changes.sql
@@ -1,3 +1,150 @@
+--14.04.2009
+
+
+ALTER TABLE billservice_periodicalservicehistory DROP COLUMN transaction_id;
+ALTER TABLE billservice_periodicalservicehistory ADD COLUMN summ double precision;
+ALTER TABLE billservice_periodicalservicehistory ADD COLUMN account_id int;
+ALTER TABLE billservice_periodicalservicehistory ADD COLUMN type_id character varying;
+ALTER TABLE billservice_periodicalservicehistory ADD CONSTRAINT billservice_periodicalservicehistory_account_id_fkey FOREIGN KEY (account_id) REFERENCES billservice_account(id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED;
+CREATE TRIGGER acc_psh_trg AFTER INSERT OR DELETE OR UPDATE ON billservice_periodicalservicehistory FOR EACH ROW EXECUTE PROCEDURE account_transaction_trg_fn(); 
+
+CREATE OR REPLACE FUNCTION periodicaltr_fn(ps_id_ integer, acctf_id_ integer, account_id_ integer, type_id_ character varying, summ_ double precision, created_ timestamp without time zone, ps_condition_type_ integer) RETURNS void
+    AS $$
+DECLARE
+    new_summ_ double precision;
+BEGIN
+    SELECT INTO new_summ_ summ_*(NOT EXISTS (SELECT id FROM billservice_suspendedperiod WHERE account_id=account_id AND (created_ BETWEEN start_date AND end_date)))::int;
+    IF (ps_condition_type_ = 1) AND (new_summ_ > 0) THEN
+        SELECT new_summ_*(ballance >= 0)::int INTO new_summ_ FROM billservice_account WHERE id=account_id_;
+    ELSIF (ps_condition_type_ = 2) AND (new_summ_ > 0) THEN
+        SELECT new_summ_*(ballance < 0)::int INTO new_summ_ FROM billservice_account WHERE id=account_id_;
+    END IF; 
+    INSERT INTO billservice_periodicalservicehistory (service_id, accounttarif_id,account_id, type_id, summ, datetime) VALUES (ps_id_, acctf_id_, account_id_, type_id_, new_summ_, created_);
+END;
+$$
+    LANGUAGE plpgsql;
+
+ALTER TABLE billservice_onetimeservicehistory ADD COLUMN summ double precision;
+ALTER TABLE billservice_onetimeservicehistory ADD COLUMN account_id int;
+ALTER TABLE billservice_onetimeservicehistory ADD CONSTRAINT billservice_onetimeservicehistory_account_id_fkey FOREIGN KEY (account_id) REFERENCES billservice_account(id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE billservice_onetimeservicehistory ADD CONSTRAINT billservice_onetimeservicehistory_onetimeservice_id_fkey FOREIGN KEY (onetimeservice_id) REFERENCES billservice_onetimeservice(id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE billservice_onetimeservicehistory DROP COLUMN transaction_id;
+CREATE TRIGGER acc_otsh_trg AFTER INSERT OR DELETE OR UPDATE ON billservice_onetimeservicehistory FOR EACH ROW EXECUTE PROCEDURE account_transaction_trg_fn();  
+
+
+ALTER TABLE billservice_traffictransaction RENAME COLUMN sum TO summ;
+  
+  
+-----------------------------------------------  
+  
+CREATE TABLE billservice_traffictransaction
+(
+  id serial NOT NULL,
+  traffictransmitservice_id integer NOT NULL,
+  account_id integer NOT NULL,
+  accounttarif_id integer NOT NULL,
+  summ double precision NOT NULL,
+  datetime timestamp with time zone NOT NULL,
+  CONSTRAINT billservice_traffictransaction_pkey PRIMARY KEY (id),
+  CONSTRAINT billservice_traffictransaction_account_id_fkey FOREIGN KEY (account_id)
+      REFERENCES billservice_account (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED,
+  CONSTRAINT billservice_traffictransaction_traffictransmitservice_id_fkey FOREIGN KEY (traffictransmitservice_id)
+      REFERENCES billservice_traffictransmitservice (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED
+)
+WITH (OIDS=FALSE);
+ALTER TABLE billservice_traffictransaction OWNER TO mikrobill;
+
+CREATE INDEX billservice_traffictransaction_account_id
+  ON billservice_traffictransaction
+  USING btree
+  (account_id);
+
+CREATE INDEX billservice_traffictransaction_traffictransmitservice_id_accounttarif_id_datetime
+  ON billservice_traffictransaction
+  USING btree
+  (traffictransmitservice_id, accounttarif_id, datetime);
+  
+
+CREATE OR REPLACE FUNCTION tftrans_ins_trg_fn() RETURNS trigger
+    AS $$ 
+BEGIN
+    NEW.datetime := date_trunc('minute', NEW.datetime) - interval '1 min' * (date_part('min', NEW.datetime)::int % 5); 
+    UPDATE billservice_traffictransaction SET summ=summ+NEW.summ WHERE traffictransmitservice_id=NEW.traffictransmitservice_id AND account_id=NEW.account_id AND datetime=NEW.datetime;
+    IF FOUND THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+    
+END;
+$$
+    LANGUAGE plpgsql;
+    
+CREATE TRIGGER tftrans_ins_trg
+    BEFORE INSERT ON billservice_traffictransaction
+    FOR EACH ROW
+    EXECUTE PROCEDURE tftrans_ins_trg_fn();
+    
+CREATE TRIGGER acc_tftrans_trg AFTER INSERT OR DELETE OR UPDATE ON billservice_traffictransaction FOR EACH ROW EXECUTE PROCEDURE account_transaction_trg_fn();
+
+CREATE TABLE billservice_timetransaction
+(
+  id serial NOT NULL,
+  timeaccessservice_id integer NOT NULL,
+  account_id integer NOT NULL,
+  accounttarif_id integer NOT NULL,
+  session_id integer NOT NULL,
+  summ double precision NOT NULL,
+  datetime timestamp with time zone NOT NULL,
+  CONSTRAINT billservice_timetransaction_pkey PRIMARY KEY (id),
+  CONSTRAINT billservice_timetransaction_account_id_fkey FOREIGN KEY (account_id)
+      REFERENCES billservice_account (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED,
+  CONSTRAINT billservice_timetransaction_timeaccessservice_id_fkey FOREIGN KEY (timeaccessservice_id)
+      REFERENCES billservice_timeaccessservice (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED,
+  CONSTRAINT billservice_timetransaction_sessionid_fkey FOREIGN KEY (session_id)
+      REFERENCES radius_session (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED
+)
+WITH (OIDS=FALSE);
+ALTER TABLE billservice_timetransaction OWNER TO mikrobill;
+
+CREATE INDEX billservice_timetransaction_account_id
+  ON billservice_timetransaction
+  USING btree
+  (account_id);
+
+CREATE INDEX billservice_timetransaction_traffictransmitservice_id_account_id_datetime
+  ON billservice_timetransaction
+  USING btree
+  (timeaccessservice_id, accounttarif_id, datetime);
+  
+
+CREATE OR REPLACE FUNCTION tmtrans_ins_trg_fn() RETURNS trigger
+    AS $$ 
+BEGIN
+    NEW.datetime := date_trunc('minute', NEW.datetime) - interval '1 min' * (date_part('min', NEW.datetime)::int % 5); 
+    UPDATE billservice_timetransaction SET summ=summ+NEW.summ WHERE timeaccessservice_id=NEW.timeaccessservice_id AND account_id=NEW.account_id AND datetime=NEW.datetime;
+    IF FOUND THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+    
+END;
+$$
+    LANGUAGE plpgsql;
+    
+CREATE TRIGGER tmtrans_ins_trg
+    BEFORE INSERT ON billservice_timetransaction
+    FOR EACH ROW
+    EXECUTE PROCEDURE tmtrans_ins_trg_fn();
+    
+CREATE TRIGGER acc_tmtrans_trg AFTER INSERT OR DELETE OR UPDATE ON billservice_timetransaction FOR EACH ROW EXECUTE PROCEDURE account_transaction_trg_fn();
+
 --06.04.2009
 ALTER TABLE billservice_shedulelog
 DROP CONSTRAINT billservice_shedulelog_accounttarif_id_fkey;
