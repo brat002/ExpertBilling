96162cf6129a9b5ddc6701675c850e216ec2b2fd
sasha <sasha>
2009-05-13 14:25:31 +0000

    core + signals t1

--- classes/core_class/AccountData.py
+++ classes/core_class/AccountData.py
@@ -1,11 +1,11 @@
 from operator import itemgetter, setitem
 
 class AccountData(tuple):
-    'AccountData(account_id, ballance, credit, datetime, tarif_id, access_parameters_id, time_access_service_id, traffic_transmit_service_id, cost, reset_tarif_cost, settlement_period_id, tarif_active, acctf_id, false_, account_created, disabled_by_limit, balance_blocked, nas_id, vpn_ip_address, ipn_ip_address, ipn_mac_address, assign_ipn_ip_from_dhcp, ipn_status, ipn_speed, vpn_speed, ipn_added, ps_null_ballance_checkout, tarif_deleted, allow_express_pay, account_status, allow_vpn_null, allow_vpn_block, username, password)' 
+    'AccountData(account_id, ballance, credit, datetime, tarif_id, access_parameters_id, time_access_service_id, traffic_transmit_service_id, cost, reset_tarif_cost, settlement_period_id, tarif_active, acctf_id, false_, account_created, disabled_by_limit, balance_blocked, nas_id, vpn_ip_address, ipn_ip_address, ipn_mac_address, assign_ipn_ip_from_dhcp, ipn_status, ipn_speed, vpn_speed, ipn_added, ps_null_ballance_checkout, tarif_deleted, allow_express_pay, account_status, allow_vpn_null, allow_vpn_block, username, password, require_tarif_cost)' 
 
     __slots__ = () 
 
-    _fields = ('account_id', 'ballance', 'credit', 'datetime', 'tarif_id', 'access_parameters_id', 'time_access_service_id', 'traffic_transmit_service_id', 'cost', 'reset_tarif_cost', 'settlement_period_id', 'tarif_active', 'acctf_id', 'false_', 'account_created', 'disabled_by_limit', 'balance_blocked', 'nas_id', 'vpn_ip_address', 'ipn_ip_address', 'ipn_mac_address', 'assign_ipn_ip_from_dhcp', 'ipn_status', 'ipn_speed', 'vpn_speed', 'ipn_added', 'ps_null_ballance_checkout', 'tarif_deleted', 'allow_express_pay', 'account_status', 'allow_vpn_null', 'allow_vpn_block', 'username', 'password') 
+    _fields = ('account_id', 'ballance', 'credit', 'datetime', 'tarif_id', 'access_parameters_id', 'time_access_service_id', 'traffic_transmit_service_id', 'cost', 'reset_tarif_cost', 'settlement_period_id', 'tarif_active', 'acctf_id', 'false_', 'account_created', 'disabled_by_limit', 'balance_blocked', 'nas_id', 'vpn_ip_address', 'ipn_ip_address', 'ipn_mac_address', 'assign_ipn_ip_from_dhcp', 'ipn_status', 'ipn_speed', 'vpn_speed', 'ipn_added', 'ps_null_ballance_checkout', 'tarif_deleted', 'allow_express_pay', 'account_status', 'allow_vpn_null', 'allow_vpn_block', 'username', 'password', 'require_tarif_cost') 
 
     def __new__(cls, account_id, ballance, credit, datetime, tarif_id, access_parameters_id, time_access_service_id, traffic_transmit_service_id, cost, reset_tarif_cost, settlement_period_id, tarif_active, acctf_id, false_, account_created, disabled_by_limit, balance_blocked, nas_id, vpn_ip_address, ipn_ip_address, ipn_mac_address, assign_ipn_ip_from_dhcp, ipn_status, ipn_speed, vpn_speed, ipn_added, ps_null_ballance_checkout, tarif_deleted, allow_express_pay, account_status, allow_vpn_null, allow_vpn_block, username, password):
         return tuple.__new__(cls, (account_id, ballance, credit, datetime, tarif_id, access_parameters_id, time_access_service_id, traffic_transmit_service_id, cost, reset_tarif_cost, settlement_period_id, tarif_active, acctf_id, false_, account_created, disabled_by_limit, balance_blocked, nas_id, vpn_ip_address, ipn_ip_address, ipn_mac_address, assign_ipn_ip_from_dhcp, ipn_status, ipn_speed, vpn_speed, ipn_added, ps_null_ballance_checkout, tarif_deleted, allow_express_pay, account_status, allow_vpn_null, allow_vpn_block, username, password)) 
@@ -14,20 +14,20 @@ class AccountData(tuple):
     def _make(cls, iterable, new=tuple.__new__, len=len):
         'Make a new AccountData object from a sequence or iterable'
         result = new(cls, iterable)
-        if len(result) != 34:
-            raise TypeError('Expected 34 arguments, got %d' % len(result))
+        if len(result) != 35:
+            raise TypeError('Expected 35 arguments, got %d' % len(result))
         return result 
 
     def __repr__(self):
-        return 'AccountData(account_id=%r, ballance=%r, credit=%r, datetime=%r, tarif_id=%r, access_parameters_id=%r, time_access_service_id=%r, traffic_transmit_service_id=%r, cost=%r, reset_tarif_cost=%r, settlement_period_id=%r, tarif_active=%r, acctf_id=%r, false_=%r, account_created=%r, disabled_by_limit=%r, balance_blocked=%r, nas_id=%r, vpn_ip_address=%r, ipn_ip_address=%r, ipn_mac_address=%r, assign_ipn_ip_from_dhcp=%r, ipn_status=%r, ipn_speed=%r, vpn_speed=%r, ipn_added=%r, ps_null_ballance_checkout=%r, tarif_deleted=%r, allow_express_pay=%r, account_status=%r, allow_vpn_null=%r, allow_vpn_block=%r, username=%r, password=%r)' % self 
+        return 'AccountData(account_id=%r, ballance=%r, credit=%r, datetime=%r, tarif_id=%r, access_parameters_id=%r, time_access_service_id=%r, traffic_transmit_service_id=%r, cost=%r, reset_tarif_cost=%r, settlement_period_id=%r, tarif_active=%r, acctf_id=%r, false_=%r, account_created=%r, disabled_by_limit=%r, balance_blocked=%r, nas_id=%r, vpn_ip_address=%r, ipn_ip_address=%r, ipn_mac_address=%r, assign_ipn_ip_from_dhcp=%r, ipn_status=%r, ipn_speed=%r, vpn_speed=%r, ipn_added=%r, ps_null_ballance_checkout=%r, tarif_deleted=%r, allow_express_pay=%r, account_status=%r, allow_vpn_null=%r, allow_vpn_block=%r, username=%r, password=%r, require_tarif_cost=%r)' % self 
 
     def _asdict(t):
         'Return a new dict which maps field names to their values'
-        return {'account_id': t[0], 'ballance': t[1], 'credit': t[2], 'datetime': t[3], 'tarif_id': t[4], 'access_parameters_id': t[5], 'time_access_service_id': t[6], 'traffic_transmit_service_id': t[7], 'cost': t[8], 'reset_tarif_cost': t[9], 'settlement_period_id': t[10], 'tarif_active': t[11], 'acctf_id': t[12], 'false_': t[13], 'account_created': t[14], 'disabled_by_limit': t[15], 'balance_blocked': t[16], 'nas_id': t[17], 'vpn_ip_address': t[18], 'ipn_ip_address': t[19], 'ipn_mac_address': t[20], 'assign_ipn_ip_from_dhcp': t[21], 'ipn_status': t[22], 'ipn_speed': t[23], 'vpn_speed': t[24], 'ipn_added': t[25], 'ps_null_ballance_checkout': t[26], 'tarif_deleted': t[27], 'allow_express_pay': t[28], 'account_status': t[29], 'allow_vpn_null': t[30], 'allow_vpn_block': t[31], 'username': t[32], 'password': t[33]} 
+        return {'account_id': t[0], 'ballance': t[1], 'credit': t[2], 'datetime': t[3], 'tarif_id': t[4], 'access_parameters_id': t[5], 'time_access_service_id': t[6], 'traffic_transmit_service_id': t[7], 'cost': t[8], 'reset_tarif_cost': t[9], 'settlement_period_id': t[10], 'tarif_active': t[11], 'acctf_id': t[12], 'false_': t[13], 'account_created': t[14], 'disabled_by_limit': t[15], 'balance_blocked': t[16], 'nas_id': t[17], 'vpn_ip_address': t[18], 'ipn_ip_address': t[19], 'ipn_mac_address': t[20], 'assign_ipn_ip_from_dhcp': t[21], 'ipn_status': t[22], 'ipn_speed': t[23], 'vpn_speed': t[24], 'ipn_added': t[25], 'ps_null_ballance_checkout': t[26], 'tarif_deleted': t[27], 'allow_express_pay': t[28], 'account_status': t[29], 'allow_vpn_null': t[30], 'allow_vpn_block': t[31], 'username': t[32], 'password': t[33], 'require_tarif_cost': t[34]} 
 
     def _replace(self, **kwds):
         'Return a new AccountData object replacing specified fields with new values'
-        result = self._make(map(kwds.pop, ('account_id', 'ballance', 'credit', 'datetime', 'tarif_id', 'access_parameters_id', 'time_access_service_id', 'traffic_transmit_service_id', 'cost', 'reset_tarif_cost', 'settlement_period_id', 'tarif_active', 'acctf_id', 'false_', 'account_created', 'disabled_by_limit', 'balance_blocked', 'nas_id', 'vpn_ip_address', 'ipn_ip_address', 'ipn_mac_address', 'assign_ipn_ip_from_dhcp', 'ipn_status', 'ipn_speed', 'vpn_speed', 'ipn_added', 'ps_null_ballance_checkout', 'tarif_deleted', 'allow_express_pay', 'account_status', 'allow_vpn_null', 'allow_vpn_block', 'username', 'password'), self))
+        result = self._make(map(kwds.pop, ('account_id', 'ballance', 'credit', 'datetime', 'tarif_id', 'access_parameters_id', 'time_access_service_id', 'traffic_transmit_service_id', 'cost', 'reset_tarif_cost', 'settlement_period_id', 'tarif_active', 'acctf_id', 'false_', 'account_created', 'disabled_by_limit', 'balance_blocked', 'nas_id', 'vpn_ip_address', 'ipn_ip_address', 'ipn_mac_address', 'assign_ipn_ip_from_dhcp', 'ipn_status', 'ipn_speed', 'vpn_speed', 'ipn_added', 'ps_null_ballance_checkout', 'tarif_deleted', 'allow_express_pay', 'account_status', 'allow_vpn_null', 'allow_vpn_block', 'username', 'password', 'require_tarif_cost'), self))
         if kwds:
             raise ValueError('Got unexpected field names: %r' % kwds.keys())
         return result 
@@ -69,3 +69,4 @@ class AccountData(tuple):
     allow_vpn_block = property(itemgetter(31))
     username = property(itemgetter(32))
     password = property(itemgetter(33))
+    require_tarif_cost = property(itemgetter(34))
