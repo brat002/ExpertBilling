e2e7f763d1491576aa35b387ff8f21d6a5895eef
egor <egor>
2009-12-17 13:55:48 +0000

    helpdesk - вход в систему

--- webadmin/ebscab/helpdesk/views.py
+++ webadmin/ebscab/helpdesk/views.py
@@ -1,1 +1,45 @@
-# Create your views here.
+#-*-coding=utf-8-*-
+from django.http import Http404, HttpResponseRedirect, HttpResponse 
+from billservice.models import SystemUser
+from billservice import authenticate, log_in, log_out
+
+from helpdesk.models import Ticket
+from helpdesk.forms import LoginForm 
+from lib.decorators import render_to
+
+@render_to('helpdesk/login.html')
+def _login(request):
+    form = LoginForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            try:
+                user = SystemUser.objects.get(username=form.cleaned_data['username'])
+                if user.password == form.cleaned_data['password']:
+                    user = authenticate(username=user.username, password=form.cleaned_data['password'])
+                    log_in(request, user)
+                    return HttpResponseRedirect('/helpdesk/')
+                else:
+                    form = LoginForm(initial={'username': form.cleaned_data['username']})
+                    return {
+                            'form':form,
+                            }
+            except :
+                form = LoginForm(initial={'username': form.cleaned_data['username']})
+                return {
+                        'form':form,
+                        }
+    else:
+        form = LoginForm(initial={'username': request.POST.get('username', None)})
+        return {
+
+                'form':form,
+                }
+        
+@render_to('helpdesk/index.html')
+def index(request):
+    return {
+            'index':'index',
+            }
+
+
+
