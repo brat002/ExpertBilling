062d1ad408266654136d0bf67c50ad678b1ec70c
andrey <andrey>
2009-12-18 12:42:53 +0000

    #857

--- helpdesk/templatetags/helpdesk_tags.py
+++ helpdesk/templatetags/helpdesk_tags.py
@@ -4,8 +4,9 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag("helpdesk/templatetags/table_tickets.html")
-def table_ticket(objects):
+def table_ticket(object_var):
     context = {}
-    context['objects'] = objects
-    print objects
+    context['object'] = object_var
+    context['object_name'] = object_var._meta.object_name
+    print context['object_name']
     return context
