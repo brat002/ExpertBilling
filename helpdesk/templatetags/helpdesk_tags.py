5650b6356a49ac7403b6612a1cd8dcfd4ae033da
andrey <andrey>
2009-12-18 14:48:04 +0000

    #857

--- helpdesk/templatetags/helpdesk_tags.py
+++ helpdesk/templatetags/helpdesk_tags.py
@@ -3,10 +3,16 @@ from django import template
 
 register = template.Library()
 
+@register.inclusion_tag("helpdesk/templatetags/pannel_ticket_table.html")
+def pannel_ticket_table(objects, objects_id):
+    context = {}
+    context['object_name'] = objects
+    context['object_id'] = objects_id
+    return context
+
 @register.inclusion_tag("helpdesk/templatetags/table_tickets.html")
 def table_ticket(object_var):
     context = {}
     context['object'] = object_var
     context['object_name'] = object_var._meta.object_name
-    print context['object_name']
     return context
