f6e7043157056873b2a7019a0a2e8f993d6a1255
andrey <andrey>
2009-12-28 13:13:25 +0000

    #857

--- media/js/auroramenu.js
+++ media/js/auroramenu.js
@@ -8,29 +8,25 @@ $(document).ready(function(){
 	
 	$.cookie('testcookie' , 'expanded')
 	$('.auroramenu').each(function(){
+			var sender = jQuery(this)
 			var tag_a = $(this).find('a.link');
-			
 			var next_tag = $(this).children('div.content');
 			if ($.cookie('arMenu_' + next_tag.attr('id')) == 1) {
 				next_tag.css("display", "none");
-				tag_a.attr('onClick', 'auroraMenuItem(\'' + this.id + '\'); return false;');
-			}
-			else {
-				tag_a.attr('onClick', 'auroraMenuItem(\'' + this.id + '\'); return false;');
 			};
+			tag_a.bind('click', function (){
+			var next_tag = sender.children('div.content')
+			if (next_tag.is(":visible")){
+				next_tag.slideUp(auroraMenuSpeed);
+				next_tag.css('display','none');
+				$.cookie('arMenu_' + next_tag.attr('id'), 1);
+			}
+			else{
+				next_tag.slideDown(auroraMenuSpeed);
+				next_tag.css('display','block');
+				$.cookie('arMenu_' + next_tag.attr('id'), 0);
+				
+			};
+			});
 	});
 });
-function auroraMenuItem(sender){
-	var next_tag = jQuery('#'+sender).children('div.content')
-
-	if (next_tag.is(":visible")){
-		next_tag.slideUp(auroraMenuSpeed);
-		next_tag.css('display','none');
-		$.cookie('arMenu_' + next_tag.attr('id'), 1);
-	}
-	else{
-		next_tag.slideDown(auroraMenuSpeed);
-		$.cookie('arMenu_' + next_tag.attr('id'), 0);
-		next_tag.css('display','block');
-	};
-};
