884a5ab4d396fe2a691111eb508584aac9425ab0
sasha <sasha>
2010-01-29 20:31:17 +0000

    ''

--- sql/changes.sql
+++ sql/changes.sql
@@ -2658,3 +2658,18 @@ $BODY$
 --06.01.2010 18:08
 ALTER TABLE billservice_account
    ADD COLUMN contract text DEFAULT '';
+   
+-- 29.01.2010 21:33
+
+ALTER TABLE billservice_ipinuse ALTER ip TYPE inet USING ip::inet;
+
+CREATE OR REPLACE FUNCTION get_free_ip_from_pool(ag_pool_id_ int) 
+  RETURNS inet AS
+$BODY$
+BEGIN
+    RETURN (SELECT free_ip.ipaddress FROM (SELECT (SELECT start_ip FROM billservice_ippool WHERE id=ag_pool_id_) + ip_series.ip_inc FROM generate_series(0, (SELECT end_ip - start_ip FROM billservice_ippool WHERE id=ag_pool_id_)) AS ip_series(ip_inc)) AS free_ip(ipaddress) WHERE free_ip.ipaddress NOT IN (SELECT ip FROM billservice_ipinuse WHERE pool_id=ag_pool_id_) LIMIT 1);
+
+END;
+$BODY$
+  LANGUAGE 'plpgsql' VOLATILE
+  COST 100;
