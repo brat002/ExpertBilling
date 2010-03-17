68f63c6d56ec84824b800baa8dc82b6f4b855266
olga <olga>
2010-03-17 14:52:44 +0000

    904 : client for ebs C# .Net 3.5 Framework win XP, Vista, Win 7

---
+++ ebsclient/Preferences.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Preferences
+{
+    public class WindowPreferences
+    {
+        public static int XPosLeft;
+        public static int YPosUpper;
+        public static bool Opacity;
+        public static int OpacityScale;
+        public static bool TopMost;
+        public static bool AutoRun;
+        public static bool SavePassword;
+        public static string Login;
+        private string _password;
+        public static string EncryptPassword;
+        public static string ServerName;
+        private bool _disconnected;
+        //private bool _isPreferences;
+
+        public int _XPosLeft
+        {
+            get { return XPosLeft; }
+            set { XPosLeft = value; }
+        }
+
+        public int _YPosUpper
+        {
+            get { return YPosUpper; }
+            set { YPosUpper = value; }
+        }
+
+        public int _OpacityValue
+        {
+            get { return OpacityScale; }
+            set { OpacityScale = value; }
+        }
+
+        public bool _Opacity
+        {
+            get { return Opacity; }
+            set { Opacity = value; }
+        }
+
+        public bool _TopMost
+        {
+            get { return TopMost; }
+            set { TopMost = value; }
+        }
+
+        public bool _AutoRun
+        {
+            get { return AutoRun; }
+            set { AutoRun = value; }
+        }
+
+        public bool _SavePassword
+        {
+            get { return SavePassword; }
+            set { SavePassword = value; }
+        }
+
+        public bool _Disconnected
+        {
+            get { return _disconnected; }
+            set { _disconnected = value; }
+        }
+
+        public bool _IsPreferences
+        {
+            get
+            {
+                if ((_SavePassword)
+                    && (_Login != String.Empty)
+                    && (this._ServerName != String.Empty))
+                    return true;
+                return false;
+            }
+        }
+
+        public string _Login
+        {
+            get { return Login; }
+            set { Login = value; }
+        }
+
+        public void SetEncryptPassword( string openText )
+        {
+            _EncryptPassword = Encrypt(openText);
+        }
+
+        public void SetDecryptPassword( string encodedText )
+        {
+            SetPassword(Decrypt(encodedText));
+        }
+      
+        public string _EncryptPassword
+        {
+            get { return EncryptPassword; }
+            set { EncryptPassword = value; }
+        }
+
+        public string GetPassword()
+        {
+            return _password;
+        }
+        public void SetPassword(string new_password)
+        {
+            _password = new_password;
+        }
+        //public string _Password
+        //{
+        //    get
+        //    {
+        //        if( _password != String.Empty )
+        //            return _password;
+                
+        //        if(EncryptPassword != String.Empty)
+        //            return Decrypt(EncryptPassword);
+                
+        //        return "";
+        //    }
+        //    set { _password = value; }
+        //}
+
+        public string _ServerName
+        {
+            get { return ServerName; }
+            set { ServerName = value; }
+        }
+
+        public void SavePreferences( int location_x, int location_y)
+        {
+            _XPosLeft = location_x;
+            _YPosUpper = location_y;
+
+            SetEncryptPassword(GetPassword());
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(WindowPreferences));
+            TextWriter textWriter = new StreamWriter("settings.xml");
+            xmlSerializer.Serialize(textWriter, this);
+
+            textWriter.Close();
+        }
+
+        /// <summary>
+        /// Encrypt a string.
+        /// </summary>
+        /// <param name="originalString">The original string.</param>
+        /// <returns>The encrypted string.</returns>
+        /// <exception cref="ArgumentNullException">This exception will be thrown when the original string is null or empty.</exception>
+        public static string Encrypt(string originalString)
+        {
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes("ZeroCool");
+
+            if (String.IsNullOrEmpty(originalString))
+            {
+                throw new ArgumentNullException("The string which needs to be encrypted can not be null.");
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateEncryptor(bytes, bytes), CryptoStreamMode.Write);
+
+            StreamWriter writer = new StreamWriter(cryptoStream);
+            writer.Write(originalString);
+            writer.Flush();
+            cryptoStream.FlushFinalBlock();
+            writer.Flush();
+
+            return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+        }
+
+        /// <summary>
+        /// Decrypt a crypted string.
+        /// </summary>
+        /// <param name="cryptedString">The crypted string.</param>
+        /// <returns>The decrypted string.</returns>
+        /// <exception cref="ArgumentNullException">This exception will be thrown when the crypted string is null or empty.</exception>
+        public static string Decrypt(string cryptedString)
+        {
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes("ZeroCool");
+
+            if (String.IsNullOrEmpty(cryptedString))
+            {
+                throw new ArgumentNullException("The string which needs to be decrypted can not be null.");
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(cryptedString));
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoProvider.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cryptoStream);
+
+            return reader.ReadToEnd();
+        }
+
+    }
+}
---
+++ ebsclient/ProfileForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Windows.Forms;
+using Preferences;
+
+namespace ebsmon
+{
+    public partial class ProfileForm : Form
+    {
+        private const string DefaultPassword = "            ";
+
+        private WindowPreferences _windowPreferences = new WindowPreferences();
+
+        public WindowPreferences _WindowPreferences
+        {
+            get { return _windowPreferences; }
+            set { _windowPreferences = value; }
+        }
+
+        public ProfileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            if (checkBoxMainWindowOpacity.Checked)
+            { _WindowPreferences._Opacity = true; }
+            else
+            { _WindowPreferences._Opacity = false; }
+            _WindowPreferences._OpacityValue = trackBarMainWindowOpacity.Value;
+
+            if( isTopmost.Checked )
+            { _WindowPreferences._TopMost = true; }
+            else 
+            { _WindowPreferences._TopMost = false; }
+            
+            if( isAutoRun.Checked )
+            { _WindowPreferences._AutoRun = true; }
+            else
+            { _WindowPreferences._AutoRun = false; }
+
+            if( isSavePassword.Checked )
+            { _WindowPreferences._SavePassword = true;}
+            else
+            { _WindowPreferences._SavePassword = false; }
+
+            if( textBoxLogin.Text != String.Empty )
+            { _WindowPreferences._Login = textBoxLogin.Text; }
+            else
+            { _WindowPreferences._Login = String.Empty; }
+
+            if( textBoxServerName.Text != String.Empty )
+            { _WindowPreferences._ServerName = textBoxServerName.Text; }
+            else
+            {
+                // always will be keeped previous state of server name
+                //_WindowPreferences._ServerName = String.Empty;
+            }
+            if( textBoxPassword.Text != String.Empty )
+            {
+                if (isSavePassword.Checked
+                    && textBoxLogin.Text != String.Empty)
+                {
+                    if (textBoxPassword.Text != DefaultPassword)
+                    {
+                        _WindowPreferences.SetPassword(textBoxPassword.Text);
+                    }
+                }
+                else
+                {
+                    _WindowPreferences._EncryptPassword = String.Empty;
+                }
+            }
+            
+            this.Close();
+        }
+
+        private void ProfileForm_Load(object sender, EventArgs e)
+        {
+            // 20 - offset between windows, not a magic number!!          
+            this.Left = _WindowPreferences._XPosLeft + 20;
+            this.Top = _WindowPreferences._YPosUpper + 20;
+
+            if ( _WindowPreferences._Opacity )
+            {
+                checkBoxMainWindowOpacity.Checked = true;
+                trackBarMainWindowOpacity.Value = Convert.ToInt32(this.Owner.Opacity * 100);
+            }
+            else
+            {
+                trackBarMainWindowOpacity.Enabled = false;
+                trackBarMainWindowOpacity.Value = _WindowPreferences._OpacityValue;
+            }
+            
+            if ( _WindowPreferences._TopMost)
+            {
+                isTopmost.Checked = true;
+            }
+            if ( _WindowPreferences._AutoRun)
+            {
+                isAutoRun.Checked = true;
+            }
+            if ( _WindowPreferences._SavePassword && _WindowPreferences._Login != String.Empty)
+            {
+                isSavePassword.Checked = true;
+            }
+            if ( _WindowPreferences._SavePassword && _WindowPreferences._EncryptPassword != String.Empty && _WindowPreferences._Login != String.Empty)
+            {
+                // for do not show decrypted password
+                textBoxPassword.Text = DefaultPassword;
+            }
+            
+            if ( _WindowPreferences._Login != String.Empty )
+            { textBoxLogin.Text = _WindowPreferences._Login; }
+            else
+            { isSavePassword.Enabled = false; }
+
+            if ( _WindowPreferences._ServerName != String.Empty )
+            {
+                textBoxServerName.Text = _WindowPreferences._ServerName;
+            }
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == String.Empty
+                || textBoxPassword.Text == String.Empty)
+                isSavePassword.Enabled = false;
+            else
+            { isSavePassword.Enabled = true; }
+        }
+
+        private void textBoxLogin_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == String.Empty 
+                || textBoxPassword.Text == String.Empty)
+                isSavePassword.Enabled = false;
+            else
+            { isSavePassword.Enabled = true; }
+        }
+
+        private void checkBoxMainWindowOpacity_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxMainWindowOpacity.Checked)
+            {
+                trackBarMainWindowOpacity.Enabled = true;
+                _WindowPreferences._Opacity = true;
+                _WindowPreferences._OpacityValue = trackBarMainWindowOpacity.Value;
+            }
+            else
+            {
+                trackBarMainWindowOpacity.Enabled = false;
+                _WindowPreferences._Opacity = false;
+                this.Owner.Opacity = 1;
+            }
+        }
+
+        private void trackBarMainWindowOpacity_Scroll(object sender, EventArgs e)
+        {
+            _WindowPreferences._OpacityValue = trackBarMainWindowOpacity.Value;
+            this.Owner.Opacity = Convert.ToDouble(trackBarMainWindowOpacity.Value) / 100;
+        }
+    }
+}
