60f246912412df82481a7e23b728d4081fa001db
dolphinik <brat002@gmail.com>
2011-03-01 16:05:35 +0000

    *bugfix

--- scripts/getintdata.py
+++ scripts/getintdata.py
@@ -38,7 +38,6 @@ nas_bytes = {}
 #########################
 snmpget='/usr/bin/snmpget'
 snmpwalk='/usr/bin/snmpwalk'
-host='10.244.0.6'
 community='public'
 db_path='/tmp/bandwidth_%s.rrd'
 db_template="/usr/bin/rrdtool create %s -s 300 DS:in:DERIVE:600:0:U DS:out:DERIVE:600:0:U RRA:AVERAGE:0.5:1:576 RRA:AVERAGE:0.5:6:672 RRA:AVERAGE:0.5:24:732 RRA:AVERAGE:0.5:144:1460" % db_path
@@ -50,7 +49,7 @@ for nas in nasses:
     session_counter=0
     bytes_in, bytes_out = 0,0
     #print "snmpwalk -v 1 -Oqs -c public %s iso.3.6.1.2.1.31.1.1.1.1" % nas
-    status, output = commands.getstatusoutput("%s -v 1 -Oqs -c public %s iso.3.6.1.2.1.31.1.1.1.1" % (snmpwalk,nas))
+    status, output = commands.getstatusoutput("%s -v 1 -Oqsn -c public %s iso.3.6.1.2.1.31.1.1.1.1" % (snmpwalk,nas))
     print status, output
     unixstamp = int(time.time())+1
     start_unixstamp = unixstamp-86400-1
@@ -71,22 +70,20 @@ for nas in nasses:
             except:
                 continue
             user_db_path = db_path % user
-    	if not os.path.isfile(user_db_path):
-    	    st, out=commands.getstatusoutput(db_template % user)
-    	    print "st=",st, out
-    	    #sys.exit()
-    
-            #print id, value
-            status, output_in = commands.getstatusoutput("snmpget -v 1 -Oqv -c public %s .1.3.6.1.2.1.2.2.1.10.%s" % (host,id))
-            status, output_out = commands.getstatusoutput("snmpget -v 1 -Oqv -c public %s .1.3.6.1.2.1.2.2.1.16.%s" % (host,id))
-            print id, value,output_in, output_out, user_db_path
-            status, output=commands.getstatusoutput("rrdtool updatev %s N:%s:%s" % (user_db_path, output_in, output_out,))
-            print "status=%s output=%s" % (status, output)
-            session_counter+=1
-            bytes_in+=output_in
-            bytes_out+=output_out
-            #continue
-            #status, output = commands.getstatusoutput("""rrdtool graph /tmp/bandwidth_%s.png -a PNG -h 125 -v "Данные о загрузке eth0" --start %s --end %s 'DEF:in=%s:in:AVERAGE' 'DEF:out=%s:out:AVERAGE' 'CDEF:kbin=in,1024,/' 'CDEF:kbout=out,1024,/' 'AREA:in#00FF00:Загрузка In' 'LINE1:out#0000FF:Загрузка Out\j' 'GPRINT:kbin:LAST:Последнее значение In\: %%3.2lf кБ-сек' 'GPRINT:kbout:LAST:Последнее значение Out\: %%3.2lfкБсек\j'""" % (user, start_unixstamp, unixstamp, user_db_path, user_db_path,))
-            print status, output
-            print "===="*10
+            if not os.path.isfile(user_db_path):
+                st, out=commands.getstatusoutput(db_template % user)
+                print "st=",st, out
+                status, output_in = commands.getstatusoutput("snmpget -v 1 -Oqv -c public %s .1.3.6.1.2.1.2.2.1.10.%s" % (host,id))
+                status, output_out = commands.getstatusoutput("snmpget -v 1 -Oqv -c public %s .1.3.6.1.2.1.2.2.1.16.%s" % (host,id))
+                print id, value,output_in, output_out, user_db_path
+                status, output=commands.getstatusoutput("rrdtool updatev %s N:%s:%s" % (user_db_path, output_in, output_out,))
+                print "status=%s output=%s" % (status, output)
+                session_counter+=1
+                bytes_in+=int(output_in)
+                bytes_out+=(output_out)
+                #continue
+                #status, output = commands.getstatusoutput("""rrdtool graph /tmp/bandwidth_%s.png -a PNG -h 125 -v "Данные о загрузке eth0" --start %s --end %s 'DEF:in=%s:in:AVERAGE' 'DEF:out=%s:out:AVERAGE' 'CDEF:kbin=in,1024,/' 'CDEF:kbout=out,1024,/' 'AREA:in#00FF00:Загрузка In' 'LINE1:out#0000FF:Загрузка Out\j' 'GPRINT:kbin:LAST:Последнее значение In\: %%3.2lf кБ-сек' 'GPRINT:kbout:LAST:Последнее значение Out\: %%3.2lfкБсек\j'""" % (user, start_unixstamp, unixstamp, user_db_path, user_db_path,))
+                print status, output
+                print "===="*10
         status, output=commands.getstatusoutput("rrdtool updatev %s N:%s:%s" % (nas_db_path, bytes_in, bytes_out,))
+
