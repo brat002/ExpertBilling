1e361613316b85712c9c5ce6d4e6111be6a6db8f
dolphinik <brat002@gmail.com>
2011-03-09 18:43:32 +0000

    +add ipv6 support +add autodetect of mac ant others

--- billservice/views.py
+++ billservice/views.py
@@ -678,24 +678,8 @@ def card_acvation(request):
         return {
                 'error_message': u'Вам не доступна услуга активации карт экспресс оплаты!',
                 }
-    if not cache.get(user.id):
-        return {
-                'redirect':'/login/',
-               }
-        #HttpResponseRedirect('/')
-    cache_user = cache.get(user.id)
-    if int(cache_user['count']) > settings.ACTIVATION_COUNT and not bool(cache_user['blocked']):
-        cache.delete(user.id)
-        cache.set(user.id, {'count':int(cache_user['count']),'last_date':cache_user['last_date'],'blocked':True,}, 86400*365)
-    if int(cache_user['count']) > settings.ACTIVATION_COUNT and bool(cache_user['blocked']):
-        time = datetime.datetime.now() - cache_user['last_date']
-        if time.seconds > settings.BLOCKED_TIME:
-            cache.delete(user.id)
-            cache.set(user.id, {'count':0,'last_date':cache_user['last_date'],'blocked':False,}, 86400*365)
-        else:
-            return {
-                    'redirect':'/',
-                   }
+
+
     if request.method == 'POST':
         form = CardForm(request.POST)
         error_message = ''
