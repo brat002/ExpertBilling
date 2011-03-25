a38fc9e6e4ccd660a63d1205732e9a87d232ef21
dolphinik <brat002@gmail.com>
2011-03-25 16:02:22 +0000

    *migration script

--- upgrade.py
+++ upgrade.py
@@ -10,6 +10,7 @@ import commands
 import ConfigParser
 import datetime
 import psycopg2
+import psycopg2.extras
 
 DIST_PATH=os.path.abspath('.')
 SQL_UPGRADE_PATH = DIST_PATH+'/sql/upgrade/' 
@@ -33,10 +34,11 @@ try:
 except Exception, e:
     print "I am unable to connect to the database"
     print e
+    print "Please, enter correct database parameters in %s/ebs_config.ini" % BILLING_PATH
     sys.exit()
         
 def modification_date(filename):
-    t = os.path.getmtime(filename)
+    t = os.path.getctime(filename)
     return datetime.datetime.fromtimestamp(t)
 
 def md5gen(file_path):
@@ -123,7 +125,7 @@ def files_for_copy():
 def backup_db():
 
     print "*"*80  
-    print "Please, enter password for database user %s.\nYou can see right password in file /opt/ebs/data/ebs_config.ini" % (config.get('db', 'username'),)
+    print "Please, enter password for DB user %s.\nYou can see right password in file /opt/ebs/data/ebs_config.ini" % (config.get('db', 'username'),)
     status, output = commands.getstatusoutput('pg_dump -W -h %s -p %s -U %s -F p -b -S ebs --disable-triggers -f %s %s' % (config.get('db', 'host'),config.get('db', 'port'),config.get('db', 'username'),"%s%s_db.sql" % (BACKUP_DIR, curdate), config.get('db', 'name')))
     if status!=0:
         print "We have error on database backup operation. %s" % output
@@ -132,7 +134,7 @@ def backup_db():
     print "*"*80
         
 def upgrade_db():
-    
+    print "upgrading DB"
     first_time=False
     if not os.path.exists(LAST_SQL):
         first_time=True
@@ -147,6 +149,7 @@ def upgrade_db():
         try:
             las_sql_id = int(last.strip())
         except Exception, e:
+            print e
             print "Last SQL id in file %s have incorrect format" % LAST_SQL 
             sys.exit()
     else:
@@ -177,7 +180,7 @@ def upgrade_db():
             
         if not not_write:
             f = open(LAST_SQL, 'w')
-            f.write(id)
+            f.write('%s' % id)
             f.close()
         conn.commit()
 
@@ -195,6 +198,69 @@ def copy_files(files):
 def post_upgrade():
     pass
 
+def upgrade_from_13():
+    print "*"*80
+    print "Migrating accounts from 1.3 to 1.4"
+    print "*"*80
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    
+    cur.execute("""
+    SELECT id, username, "password", fullname, email, address, nas_id, vpn_ip_address, 
+           assign_ipn_ip_from_dhcp, ipn_ip_address, ipn_mac_address, ipn_status, 
+           status, suspended, created, ballance, credit, disabled_by_limit, 
+           balance_blocked, ipn_speed, vpn_speed, ipn_added, city, 
+           postcode, region, street, house, house_bulk, entrance, room, 
+           vlan, allow_webcab, allow_expresscards, assign_dhcp_null, assign_dhcp_block, 
+           allow_vpn_null, allow_vpn_block, passport, passport_date, passport_given, 
+           phone_h, phone_m, vpn_ipinuse_id, ipn_ipinuse_id, associate_pptp_ipn_ip, associate_pppoe_mac
+      FROM billservice_account;
+    """)
+    accounts = cur.fetchall()
+    
+    for account in accounts:
+        cur.execute(""" 
+        INSERT INTO billservice_subaccount(
+                account_id, username, "password", vpn_ip_address, ipn_ip_address, 
+                ipn_mac_address, nas_id, ipn_added, ipn_enabled, 
+                allow_dhcp, allow_dhcp_with_null, 
+                allow_dhcp_with_minus, allow_dhcp_with_block, allow_vpn_with_null, 
+                allow_vpn_with_minus, allow_vpn_with_block, associate_pptp_ipn_ip, 
+                associate_pppoe_ipn_mac, ipn_speed, vpn_speed, allow_addonservice, 
+                vpn_ipinuse_id, ipn_ipinuse_id, allow_ipn_with_null, 
+                allow_ipn_with_minus, allow_ipn_with_block)
+        VALUES (%s, %s, %s, %s, %s, 
+                %s, %s, %s, %s, %s, 
+                %s, %s, %s, %s, %s, 
+                %s, %s, %s, 
+                %s, %s, %s, 
+                %s, %s, %s, %s, 
+                %s);    
+        """, (account['id'],account['username'],account['password'],account['vpn_ip_address'],account['ipn_ip_address'],
+              account['ipn_mac_address'],account['nas_id'],account['ipn_added'],account['ipn_status'],
+              account['assign_ipn_ip_from_dhcp'],account['assign_dhcp_null'],
+              account['assign_dhcp_null'],account['assign_dhcp_block'],account['allow_vpn_null'],
+              account['allow_vpn_null'],account['allow_vpn_block'],account['associate_pptp_ipn_ip'],
+              account['associate_pppoe_mac'],account['ipn_speed'],account['vpn_speed'],True,
+              account['vpn_ipinuse_id'],account['ipn_ipinuse_id'], False, False, False, 
+              ))
+    
+    cur.execute("UPDATE billservice_accountaddonservice as accs SET account_id=Null, subaccount_id=(SELECT id FROM billservice_subaccount  WHERE account_id=accs.account_id LIMIT 1) WHERE accs.deactivated is Null")
+    cur.execute("UPDATE billservice_account set nas_id = Null")
+    conn.commit()
+    print '*'*80
+    print "Migration completed"
+    
+    
+def import_dump():
+    print "*"*80
+    print "Importing main database dump.Enter database password for user %s" % config.get('db', 'username')
+    print "*"*80
+    status, output = commands.getstatusoutput('psql -W -h %s -p %s -U %s %s -f %s' % (config.get('db', 'host'),config.get('db', 'port'),config.get('db', 'username'), config.get('db', 'name'), DIST_PATH+'/sql/ebs_dump.sql'))
+    
+    if status!=0:
+        allow_continue("We get error when importing initial dump. %s" % output)
+        
+        
 def fromchanges():
     
     to_db=[]
@@ -213,30 +279,50 @@ def fromchanges():
             changes_start=True
             to_db.append(line)
              
-    print '\n'.join(to_db)   
+    #print '\n'.join(to_db)   
     try:
         cur.execute('\n'.join(to_db))
         conn.commit()
     except Exception, e:
-        print e 
-        conn.rollback()
-        print "*"*80
-        print "Error First stage upgrading DB"
-        allow_continue()
+        print e
+    
+        #print e 
+        #conn.rollback()
+    print "*"*80
+        #print "Error First stage upgrading DB"
+        #allow_continue()
+    print 'First stage DB upgrading was completed'
+    conn.commit()
 
-installation_date = modification_date(BILLING_PATH+'/license.lic')
-print installation_date
-stop_processes()
-pre_upgrade()
-files=files_for_copy()
-if not files:
-    print '*'*80
-    print 'No files copy needed'
-    allow_continue('Do you want to upgrade EBS database?')
+if __name__=='__main__':
+    installation_date = modification_date(BILLING_PATH+'/license.lic')
+    
+    if 'install' in sys.argv:
+        import_dump()
 
-copy_files(files)  
-backup_db()
-if not os.path.exists(LAST_SQL):
-    fromchanges()
-upgrade_db()
-#start_processes()
+        fromchanges()
+        upgrade_db()
+    if  'upgrade' in sys.argv:
+    #print installation_date
+        stop_processes()
+        pre_upgrade()
+        files=files_for_copy()
+        if not files:
+            print '*'*80
+            print 'No files copy needed'
+            allow_continue('Do you want to upgrade EBS database?')
+    
+        copy_files(files)  
+        backup_db()
+        fromchanges()
+        upgrade_db()
+
+    
+    
+    if 'migrate' in sys.argv:
+        allow_continue('Do you want to migrate your accounts database from 1.3 to 1.4 EBS version?')
+        backup_db()
+        upgrade_db()
+        upgrade_from_13()
+    
+    #start_processes()
