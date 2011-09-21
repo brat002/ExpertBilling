0f71a68530283dcb198f615550f9e555d89bd9bc
dolphinik <brat002@gmail.com>
2011-09-21 14:42:55 +0000

    123

--- option_parser.py
+++ option_parser.py
@@ -6,8 +6,8 @@ def parse(nas_type, remote_id, circuit_id):
     if nas_type=='dlink-32xx':
         port = struct.unpack("!B",circuit_id[-1])[0]
         
-        length=struct.unpack("!B",remote_id[3])
-        identify=struct.unpack("!%ss" % length,remote_id[4:])[0]
+        length=struct.unpack("!B",remote_id[1])[0]
+        identify=struct.unpack("!%ss" % length,remote_id[2:])[0]
         
     return identify, vlan, module, port
         
