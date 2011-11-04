4726f0652fa0e40e912df7941d6160dbcda85389
dolphinik <brat002@gmail.com>
2011-11-04 14:59:27 +0000

    *guest pool bugfix

--- classes/core_class/RadiusSession.py
+++ classes/core_class/RadiusSession.py
@@ -1,33 +1,33 @@
 from operator import itemgetter, setitem
 
 class RadiusSession(tuple):
-    'RadiusSession(id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start, ipinuse_id)' 
+    'RadiusSession(id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start, ipinuse_id, caller_id, guest_pool)' 
 
     __slots__ = () 
 
-    _fields = ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address', 'speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id') 
+    _fields = ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address', 'speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id', 'guest_pool') 
 
-    def __new__(cls, id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id):
-        return tuple.__new__(cls, (id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id)) 
+    def __new__(cls, id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id,guest_pool):
+        return tuple.__new__(cls, (id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id,guest_pool)) 
 
     @classmethod
     def _make(cls, iterable, new=tuple.__new__, len=len):
         'Make a new RadiusSession object from a sequence or iterable'
         result = new(cls, iterable)
-        if len(result) != 12:
-            raise TypeError('Expected 12 arguments, got %d' % len(result))
+        if len(result) != 13:
+            raise TypeError('Expected 13 arguments, got %d' % len(result))
         return result 
 
     def __repr__(self):
-        return 'RadiusSession(id=%r, account_id=%r, subaccount_id=%r, sessionid=%r, framed_ip_address=%r, speed_string=%r, access_type=%r, nas_id=%r, time_from_last_update=%r, date_start=%r, ipinuse_id=%r, caller_id=%r)' % self 
+        return 'RadiusSession(id=%r, account_id=%r, subaccount_id=%r, sessionid=%r, framed_ip_address=%r, speed_string=%r, access_type=%r, nas_id=%r, time_from_last_update=%r, date_start=%r, ipinuse_id=%r, caller_id=%r, guest_pool=%r)' % self 
 
     def _asdict(t):
         'Return a new dict which maps field names to their values'
-        return {'id': t[0], 'account_id': t[1], 'subaccount_id':t[2], 'sessionid': t[3], 'framed_ip_address':t[4], 'speed_string': t[5], 'access_type': t[6], 'nas_id': t[7], 'time_from_last_update':t[8], 'date_start':t[9], 'ipinuse_id':t[10], 'caller_id':t[11]} 
+        return {'id': t[0], 'account_id': t[1], 'subaccount_id':t[2], 'sessionid': t[3], 'framed_ip_address':t[4], 'speed_string': t[5], 'access_type': t[6], 'nas_id': t[7], 'time_from_last_update':t[8], 'date_start':t[9], 'ipinuse_id':t[10], 'caller_id':t[11], 'guest_pool':t[12]} 
 
     def _replace(self, **kwds):
         'Return a new RadiusSession object replacing specified fields with new values'
-        result = self._make(map(kwds.pop, ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address','speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id'), self))
+        result = self._make(map(kwds.pop, ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address','speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id', 'guest_pool'), self))
         if kwds:
             raise ValueError('Got unexpected field names: %r' % kwds.keys())
         return result 
@@ -47,3 +47,5 @@ class RadiusSession(tuple):
     date_start = property(itemgetter(9))
     ipinuse_id = property(itemgetter(10))
     caller_id = property(itemgetter(11))
+    guest_pool = property(itemgetter(12))
+    
