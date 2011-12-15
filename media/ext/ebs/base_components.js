6f0d144317cb26f3e234ab1c7fe3f1aad0898b5c
dolphinik <brat002@gmail.com>
2011-12-15 15:13:22 +0000

    *transactionreport change

--- media/ext/ebs/base_components.js
+++ media/ext/ebs/base_components.js
@@ -465,7 +465,7 @@ Ext.ux.form.DateTime = Ext.extend(Ext.form.Field, {
         		
         		
         		if(me.win.quarter.getValue()==1){
-        			quarter=1
+        			quarter=0
         		}else{
         			quarter=(me.win.quarter.getValue()-1)*3
         		}
@@ -1256,8 +1256,8 @@ Ext.extend(Ext.data.DynamicJsonReader, Ext.data.JsonReader, {
 Ext.grid.DynamicColumnModel = function(store)
 {
   var cols = [];
-  var recordType = store.recordType;
-  var fields = recordType.prototype.fields;
+  //var recordType = store.recordType;
+  //var fields = recordType.prototype.fields;
   if (store.reader.jsonData){
 	  //for dynamic columns we need to return the columnInfo from server so we can build the columns here.
 	  //in this example, the ResultData is a JSON object, returned from the server which contains a ColumnInfo
@@ -1310,3 +1310,70 @@ Ext.Component.override({
     }
 });
 
+Ext.override(Ext.data.Store,{
+	addField: function(field){
+		field = new Ext.data.Field(field);
+		this.recordType.prototype.fields.replace(field);
+		if(typeof field.defaultValue != 'undefined'){
+			this.each(function(r){
+				if(typeof r.data[field.name] == 'undefined'){
+					r.data[field.name] = field.defaultValue;
+				}
+			});
+		}
+	},
+	removeField: function(name){
+		this.recordType.prototype.fields.removeKey(name);
+		this.each(function(r){
+			delete r.data[name];
+			if(r.modified){
+				delete r.modified[name];
+			}
+		});
+	}
+});
+Ext.override(Ext.grid.ColumnModel,{
+	addColumn: function(column, colIndex){
+		if(typeof column == 'string'){
+			column = {header: column, dataIndex: column};
+		}
+		var config = this.config;
+		this.config = [];
+		if(typeof colIndex == 'number'){
+			config.splice(colIndex, 0, column);
+		}else{
+			colIndex = config.push(column);
+		}
+		this.setConfig(config);
+		return colIndex;
+	},
+	removeColumn: function(colIndex){
+		var config = this.config;
+		this.config = [config[colIndex]];
+		config.splice(colIndex, 1);
+		this.setConfig(config);
+	}
+});
+Ext.override(Ext.grid.GridPanel,{
+	addColumn: function(field, column, colIndex){
+		if(!column){
+			if(field.dataIndex){
+				column = field;
+				field = field.dataIndex;
+			} else{
+				column = field.name || field;
+			}
+		}
+		this.store.addField(field);
+		return this.colModel.addColumn(column, colIndex);
+	},
+	removeColumn: function(name, colIndex){
+		this.store.removeField(name);
+		if(typeof colIndex != 'number'){
+			colIndex = this.colModel.findColumnIndex(name);
+		}
+		if(colIndex >= 0){
+			this.colModel.removeColumn(colIndex);
+		}
+	}
+});
