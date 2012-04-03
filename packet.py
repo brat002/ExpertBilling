772bd1884349c3e7d4d06518425dbe7a84429c7d
dolphinik <brat002@gmail.com>
2012-04-03 12:04:27 +0000

    * bugfix

--- packet.py
+++ packet.py
@@ -149,9 +149,9 @@ class Packet(UserDict.UserDict):
 
 	def __getitem__(self, key):
 		if type(key)!=types.StringType:
-			return self.data.get(key,'')
+			return self.data[key]
 
-		values=self.data.get(self._EncodeKey(key), "")
+		values=self.data.get(self._EncodeKey(key), [''])
 		attr=self.dict.attributes[key]
 		res=[]
 		for v in values:
