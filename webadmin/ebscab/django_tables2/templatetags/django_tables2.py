6a72f84d1368e3a5f7875dd0d05a06b293e2aa32
dolphinik <brat002@gmail.com>
2012-11-28 20:48:29 +0000

    + add customized django tables2

---
+++ webadmin/ebscab/django_tables2/templatetags/django_tables2.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from django import template
+from django.template import TemplateSyntaxError, Variable, Node
+from django.template.loader import get_template, select_template
+from django.template.defaultfilters import stringfilter, title as old_title
+from django.utils.datastructures import SortedDict
+from django.utils.http import urlencode
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+import django_tables2 as tables
+import re
+import StringIO
+import tokenize
+
+
+register = template.Library()
+kwarg_re = re.compile(r"(?:(.+)=)?(.+)")
+
+
+def token_kwargs(bits, parser):
+    """
+    Based on Django's ``django.template.defaulttags.token_kwargs``, but with a
+    few changes:
+
+    - No legacy mode.
+    - Both keys and values are compiled as a filter
+    """
+    if not bits:
+        return {}
+    kwargs = SortedDict()
+    while bits:
+        match = kwarg_re.match(bits[0])
+        if not match or not match.group(1):
+            return kwargs
+        key, value = match.groups()
+        del bits[:1]
+        kwargs[parser.compile_filter(key)] = parser.compile_filter(value)
+    return kwargs
+
+
+class SetUrlParamNode(Node):
+    def __init__(self, changes):
+        super(SetUrlParamNode, self).__init__()
+        self.changes = changes
+
+    def render(self, context):
+        request = context.get('request', None)
+        if not request:
+            return ""
+        params = dict(request.GET)
+        for key, newvalue in self.changes.items():
+            newvalue = newvalue.resolve(context)
+            if newvalue == '' or newvalue is None:
+                params.pop(key, False)
+            else:
+                params[key] = unicode(newvalue)
+        return "?" + urlencode(params, doseq=True)
+
+
+@register.tag
+def set_url_param(parser, token):
+    """
+    Creates a URL (containing only the querystring [including "?"]) based on
+    the current URL, but updated with the provided keyword arguments.
+
+    Example::
+
+        {% set_url_param name="help" age=20 %}
+        ?name=help&age=20
+
+    **Deprecated** as of 0.7.0, use ``querystring``.
+    """
+    bits = token.contents.split()
+    qschanges = {}
+    for i in bits[1:]:
+        try:
+            key, value = i.split('=', 1)
+            key = key.strip()
+            value = value.strip()
+            key_line_iter = StringIO.StringIO(key).readline
+            keys = list(tokenize.generate_tokens(key_line_iter))
+            if keys[0][0] == tokenize.NAME:
+                # workaround bug #5270
+                value = Variable(value) if value == '""' else parser.compile_filter(value)
+                qschanges[str(key)] = value
+            else:
+                raise ValueError
+        except ValueError:
+            raise TemplateSyntaxError("Argument syntax wrong: should be"
+                                      "key=value")
+    return SetUrlParamNode(qschanges)
+
+
+class QuerystringNode(Node):
+    def __init__(self, params):
+        super(QuerystringNode, self).__init__()
+        self.params = params
+
+    def render(self, context):
+        request = context.get('request', None)
+        if not request:
+            return ""
+        params = dict(request.GET)
+        for key, value in self.params.iteritems():
+            key = key.resolve(context)
+            value = value.resolve(context)
+            if key not in ("", None):
+                params[key] = value
+        return escape("?" + urlencode(params, doseq=True))
+
+
+# {% querystring "name"="abc" "age"=15 %}
+@register.tag
+def querystring(parser, token):
+    """
+    Creates a URL (containing only the querystring [including "?"]) derived
+    from the current URL's querystring, by updating it with the provided
+    keyword arguments.
+
+    Example (imagine URL is ``/abc/?gender=male&name=Brad``)::
+
+        {% querystring "name"="Ayers" "age"=20 %}
+        ?name=Ayers&gender=male&age=20
+
+    """
+    bits = token.split_contents()
+    tag = bits.pop(0)
+    try:
+        return QuerystringNode(token_kwargs(bits, parser))
+    finally:
+        # ``bits`` should now be empty, if this is not the case, it means there
+        # was some junk arguments that token_kwargs couldn't handle.
+        if bits:
+            raise TemplateSyntaxError("Malformed arguments to '%s'" % tag)
+
+
+class RenderTableNode(Node):
+    """
+    :param    table: the table to render
+    :type     table: Table object
+    :param template: Name[s] of template to render
+    :type  template: unicode or list
+    """
+    def __init__(self, table, template=None):
+        super(RenderTableNode, self).__init__()
+        self.table = table
+        self.template = template
+
+    def render(self, context):
+        table = self.table.resolve(context)
+
+        if not isinstance(table, tables.Table):
+            raise ValueError("Expected Table object, but didn't find one.")
+
+        if self.template:
+            template = self.template.resolve(context)
+        else:
+            template = table.template
+
+        if isinstance(template, basestring):
+            template = get_template(template)
+        else:
+            # assume some iterable was given
+            template = select_template(template)
+
+        # Contexts are basically a `MergeDict`, when you `update()`, it
+        # internally just adds a dict to the list to attempt lookups from. This
+        # is why we're able to `pop()` later.
+        context.update({"table": table})
+        try:
+            # HACK:
+            # TemplateColumn benefits from being able to use the context
+            # that the table is rendered in. The current way this is
+            # achieved is to temporarily attach the context to the table,
+            # which TemplateColumn then looks for and uses.
+            table.context = context
+            return template.render(context)
+        finally:
+            del table.context
+            context.pop()
+
+@register.tag
+def render_table(parser, token):
+    bits = token.split_contents()
+    try:
+        tag, table = bits.pop(0), parser.compile_filter(bits.pop(0))
+    except ValueError:
+        raise TemplateSyntaxError(u"'%s' must be given a table." % bits[0])
+    template = parser.compile_filter(bits.pop(0)) if bits else None
+    return RenderTableNode(table, template)
+
+
+class NoSpacelessNode(Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        return mark_safe(re.sub(r'>\s+<', '>&#32;<',
+                                self.nodelist.render(context)))
+
+@register.tag
+def nospaceless(parser, token):
+    nodelist = parser.parse(('endnospaceless',))
+    parser.delete_first_token()
+    return NoSpacelessNode(nodelist)
+
+
+RE_UPPERCASE = re.compile('[A-Z]')
+
+
+@register.filter
+@stringfilter
+def title(value):
+    """
+    A slightly better title template filter.
+
+    Same as Django's builtin ``title`` filter, but operates on individual words
+    and leaves words unchanged if they already have a capital letter.
+    """
+    title_word = lambda w: w if RE_UPPERCASE.search(w) else old_title(w)
+    return re.sub('(\S+)', lambda m: title_word(m.group(0)), value)
+title.is_safe = True
+
+@register.filter
+def get_sessions_class(value):
+    """
+    A slightly better title template filter.
+
+    Same as Django's builtin ``title`` filter, but operates on individual words
+    and leaves words unchanged if they already have a capital letter.
+    """
+    
+    if value.session_status=='ACK':
+        return "alert-info"
+    elif value.session_status=='ACTIVE':
+        return "alert-success"
+    else:
+        return "alert-error"
+get_sessions_class.is_safe = True
---
+++ webadmin/ebscab/django_tables2/utils.py
@@ -0,0 +1,369 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from itertools import chain
+
+
+class Sequence(list):
+    """
+    Represents a column sequence, e.g. ("first_name", "...", "last_name")
+
+    This is used to represent ``Table.Meta.sequence`` or the Table
+    constructors's ``sequence`` keyword argument.
+
+    The sequence must be a list of column names and is used to specify the
+    order of the columns on a table. Optionally a "..." item can be inserted,
+    which is treated as a *catch-all* for column names that aren't explicitly
+    specified.
+    """
+    def expand(self, columns):
+        """
+        Expands the "..." item in the sequence into the appropriate column
+        names that should be placed there.
+
+        :raises: ``ValueError`` if the sequence is invalid for the columns.
+        """
+        # validation
+        if self.count("...") > 1:
+            raise ValueError("'...' must be used at most once in a sequence.")
+        elif "..." in self:
+            # Check for columns in the sequence that don't exist in *columns*
+            extra = (set(self) - set(("...", ))).difference(columns)
+            if extra:
+                raise ValueError("sequence contains columns that do not exist"
+                                 " in the table. Remove '%s'."
+                                 % "', '".join(extra))
+        else:
+            diff = set(self) ^ set(columns)
+            if diff:
+                raise ValueError("sequence does not match columns. Fix '%s' "
+                                 "or possibly add '...'." % "', '".join(diff))
+        # everything looks good, let's expand the "..." item
+        columns = columns[:]  # don't modify
+        head = []
+        tail = []
+        target = head  # start by adding things to the head
+        for name in self:
+            if name == "...":
+                # now we'll start adding elements to the tail
+                target = tail
+                continue
+            else:
+                target.append(columns.pop(columns.index(name)))
+        self[:] = list(chain(head, columns, tail))
+
+
+class OrderBy(str):
+    """
+    A single item in an :class:`.OrderByTuple` object. This class is
+    essentially just a :class:`str` with some extra properties.
+    """
+    @property
+    def bare(self):
+        """
+        Return the bare form.
+
+        The *bare form* is the non-prefixed form. Typically the bare form is
+        just the ascending form.
+
+        Example: ``age`` is the bare form of ``-age``
+
+        :rtype: :class:`.OrderBy` object
+        """
+        return OrderBy(self[1:]) if self[:1] == '-' else self
+
+    @property
+    def opposite(self):
+        """
+        Return an :class:`.OrderBy` object with an opposite sort influence.
+
+        Example:
+
+        .. code-block:: python
+
+            >>> order_by = OrderBy('name')
+            >>> order_by.opposite
+            '-name'
+
+        :rtype: :class:`.OrderBy` object
+        """
+        return OrderBy(self[1:]) if self.is_descending else OrderBy('-' + self)
+
+    @property
+    def is_descending(self):
+        """
+        Return :const:`True` if this object induces *descending* ordering
+
+        :rtype: :class:`bool`
+        """
+        return self.startswith('-')
+
+    @property
+    def is_ascending(self):
+        """
+        Return :const:`True` if this object induces *ascending* ordering.
+
+        :returns: :class:`bool`
+        """
+        return not self.is_descending
+
+
+class OrderByTuple(tuple):
+    """Stores ordering as (as :class:`.OrderBy` objects). The
+    :attr:`django_tables2.tables.Table.order_by` property is always converted
+    to an :class:`.OrderByTuple` object.
+
+    This class is essentially just a :class:`tuple` with some useful extras.
+
+    Example:
+
+    .. code-block:: python
+
+        >>> x = OrderByTuple(('name', '-age'))
+        >>> x['age']
+        '-age'
+        >>> x['age'].is_descending
+        True
+        >>> x['age'].opposite
+        'age'
+
+    """
+    def __new__(cls, iterable):
+        transformed = []
+        for item in iterable:
+            if not isinstance(item, OrderBy):
+                item = OrderBy(item)
+            transformed.append(item)
+        return super(OrderByTuple, cls).__new__(cls, transformed)
+
+    def __unicode__(self):
+        """Human readable format."""
+        return ','.join(self)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __contains__(self, name):
+        """
+        Determine if a column has an influence on ordering.
+
+        Example:
+
+        .. code-block:: python
+
+            >>> ordering =
+            >>> x = OrderByTuple(('name', ))
+            >>> 'name' in  x
+            True
+            >>> '-name' in x
+            True
+
+        :param name: The name of a column. (optionally prefixed)
+        :returns: :class:`bool`
+        """
+        name = OrderBy(name).bare
+        for order_by in self:
+            if order_by.bare == name:
+                return True
+        return False
+
+    def __getitem__(self, index):
+        """
+        Allows an :class:`.OrderBy` object to be extracted via named or integer
+        based indexing.
+
+        When using named based indexing, it's fine to used a prefixed named.
+
+        .. code-block:: python
+
+            >>> x = OrderByTuple(('name', '-age'))
+            >>> x[0]
+            'name'
+            >>> x['age']
+            '-age'
+            >>> x['-age']
+            '-age'
+
+        :rtype: :class:`.OrderBy` object
+        """
+        if isinstance(index, basestring):
+            for order_by in self:
+                if order_by == index or order_by.bare == index:
+                    return order_by
+            raise KeyError
+        return super(OrderByTuple, self).__getitem__(index)
+
+    @property
+    def cmp(self):
+        """
+        Return a function for use with :meth:`list.sort()` that implements this
+        object's ordering. This is used to sort non-:class:`QuerySet` based
+        :term:`table data`.
+
+        :rtype: function
+        """
+        # pylint: disable=C0103
+        def _cmp(a, b):
+            for accessor, reverse in instructions:
+                x = accessor.resolve(a)
+                y = accessor.resolve(b)
+                try:
+                    res = cmp(x, y)
+                except TypeError:
+                    res = cmp((repr(type(x)), id(type(x)), x),
+                              (repr(type(y)), id(type(y)), y))
+                if res != 0:
+                    return -res if reverse else res
+            return 0
+        instructions = []
+        for order_by in self:
+            if order_by.startswith('-'):
+                instructions.append((Accessor(order_by[1:]), True))
+            else:
+                instructions.append((Accessor(order_by), False))
+        return _cmp
+
+    def get(self, key, fallback):
+        """
+        Identical to __getitem__, but supports fallback value.
+        """
+        try:
+            return self[key]
+        except (KeyError, IndexError):
+            return fallback
+
+    @property
+    def opposite(self):
+        """
+        Return version with each :class:`OrderBy` prefix toggled.
+
+        Example:
+
+        .. code-block:: python
+
+            >>> order_by = OrderByTuple(('name', '-age'))
+            >>> order_by.opposite
+            ('-name', 'age')
+        """
+        return type(self)((o.opposite for o in self))
+
+
+class Accessor(str):
+    """
+    A string describing a path from one object to another via attribute/index
+    accesses. For convenience, the class has an alias ``A`` to allow for more concise code.
+
+    Relations are separated by a ``.`` character.
+    """
+    SEPARATOR = '.'
+
+    def resolve(self, context):
+        """
+        Return an object described by the accessor by traversing the attributes
+        of *context*.
+
+        Example:
+
+        .. code-block:: python
+
+            >>> x = Accessor('__len__`')
+            >>> x.resolve('brad')
+            4
+            >>> x = Accessor('0.upper')
+            >>> x.resolve('brad')
+            'B'
+
+        :type context: :class:`object`
+        :param context: The root/first object to traverse.
+        :returns: target object
+        :raises: TypeError, AttributeError, KeyError, ValueError
+
+        :meth:`~.Accessor.resolve` attempts lookups in the following order:
+
+        - dictionary (e.g. ``obj[related]``)
+        - attribute (e.g. ``obj.related``)
+        - list-index lookup (e.g. ``obj[int(related)]``)
+
+        Callable objects are called, and their result is used, before
+        proceeding with the resolving.
+        """
+        current = context
+        for bit in self.bits:
+            try:  # dictionary lookup
+                current = current[bit]
+            except (TypeError, AttributeError, KeyError):
+                try:  # attribute lookup
+                    current = getattr(current, bit)
+                except (TypeError, AttributeError):
+                    try:  # list-index lookup
+                        current = current[int(bit)]
+                    except (IndexError,  # list index out of range
+                            ValueError,  # invalid literal for int()
+                            KeyError,    # dict without `int(bit)` key
+                            TypeError,   # unsubscriptable object
+                            ):
+                        raise ValueError('Failed lookup for key [%s] in %r'
+                                         ', when resolving the accessor %s'
+                                          % (bit, current, self))
+            if callable(current):
+                current = current()
+            # important that we break in None case, or a relationship
+            # spanning across a null-key will raise an exception in the
+            # next iteration, instead of defaulting.
+            if current is None:
+                break
+        return current
+
+    @property
+    def bits(self):
+        if self == '':
+            return ()
+        return self.split(self.SEPARATOR)
+
+
+A = Accessor  # alias
+
+class AttributeDict(dict):
+    """
+    A wrapper around :class:`dict` that knows how to render itself as HTML
+    style tag attributes.
+
+    The returned string is marked safe, so it can be used safely in a template.
+    See :meth:`.as_html` for a usage example.
+    """
+    def as_html(self):
+        """
+        Render to HTML tag attributes.
+
+        Example:
+
+        .. code-block:: python
+
+            >>> from django_tables2.utils import AttributeDict
+            >>> attrs = AttributeDict({'class': 'mytable', 'id': 'someid'})
+            >>> attrs.as_html()
+            'class="mytable" id="someid"'
+
+        :rtype: :class:`~django.utils.safestring.SafeUnicode` object
+
+        """
+        return mark_safe(' '.join(['%s="%s"' % (k, escape(v))
+                                   for k, v in self.iteritems()]))
+
+
+class Attrs(dict):
+    """
+    A collection of :class:`AttributeDict`, each given a key.
+
+    This class is used as a container to hold differenct sets of attributes for
+    a given column. Keys indicate where the attributes should be used, and
+    support varies depending on the column.
+
+    It's used in favour of a standard `dict` to enable backwards compatibility.
+    Before it was introduced, columns had an `attrs` parameter that would be
+    given a `dict` and would assign it to a single (typically input) element.
+    The new approach allows attributes to be specified for multiple elements.
+    By using the `Attrs` class your intention to use the new mechanism is
+    explicit.
+    """
