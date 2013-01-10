82ac623aa454c464549014f9c69fc900a7671ae1
dolphinik <brat002@gmail.com>
2013-01-10 13:11:39 +0000

    *bugfix

--- billservice/management/__init__.py
+++ billservice/management/__init__.py
@@ -10,15 +10,14 @@ def alter_django_auth_permissions(sender, **kwargs):
     cursor=connection.cursor()
     cursor.execute("SELECT * FROM auth_permission LIMIT 1")
     
-    print 123
+
     for desc in cursor.description:
         # See http://www.python.org/dev/peps/pep-0249/
         name, type_code, display_size, internal_size, precision, scale, null_ok = desc
         if not name=='name':
             continue
         if internal_size<SIZE_NAME:
-            logging.info('auth_permission: Column "name" gets altered. Old: %d new: %d' % (
-                    internal_size, SIZE_NAME))
+
             cursor.execute('''ALTER TABLE auth_permission ALTER COLUMN "name" type VARCHAR(%s)''',
                            [SIZE_NAME])
         break
