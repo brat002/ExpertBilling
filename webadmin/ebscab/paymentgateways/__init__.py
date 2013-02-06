527821d3a501862c17df614dc669b5a1571f3fef
dolphinik <brat002@gmail.com>
2013-02-06 20:58:34 +0000

    * make small rows in tables + add report to report system

---
+++ webadmin/ebscab/paymentgateways/__init__.py
@@ -0,0 +1,20 @@
+class PaymentSystem(object):
+    
+    
+    def __init__(self):
+        pass
+    
+    def parse_response(self):
+        pass
+    
+    def check_sign(self):
+        pass
+    
+    def sign(self):
+        pass
+    
+    def check(self):
+        pass
+    
+    def pay(self):
+        pass
