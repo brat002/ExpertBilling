db6bbb56385b5b73eb4d5df013abacb517cbabe0
dolphinik <brat002@gmail.com>
2013-02-11 21:19:21 +0000

    * changed splitted datetime widget to datetimepicker widget

--- webadmin/ebscab/radius/forms.py
+++ webadmin/ebscab/radius/forms.py
@@ -8,6 +8,6 @@ class SessionFilterForm(forms.Form):
     account = AutoCompleteSelectMultipleField( 'account_fts', required = False) 
     nas = forms.ModelMultipleChoiceField(label=u"Сервер доступа субаккаунта", queryset=Nas.objects.all(), required=False)
     id = forms.IntegerField(required=False)
-    date_start = forms.DateTimeField(label = u'С даты', required=False, widget = SplitDateTimeWidget(date_attrs={'class':'input-small datepicker'}, time_attrs={'class':'input-small timepicker'}))
-    date_end = forms.DateTimeField(label = u'По дату', required=False, widget = SplitDateTimeWidget(date_attrs={'class':'input-small datepicker'}, time_attrs={'class':'input-small timepicker'}))
+    date_start = forms.DateTimeField(label = u'С даты', required=False, widget = forms.widgets.DateTimeInput(attrs={'class':'datepicker'}))
+    date_end = forms.DateTimeField(label = u'По дату', required=False, widget = forms.widgets.DateTimeInput(attrs={'class':'datepicker'}))
     only_active = forms.BooleanField(label = u'Только активные', widget = forms.widgets.CheckboxInput, required=False)
