d034c981d721c333cdc6a90785a109573c1488ac
dolphinik <brat002@gmail.com>
2013-03-12 12:13:13 +0000

    + add del to requirements

--- fabfile.py
+++ fabfile.py
@@ -45,6 +45,7 @@ def configure_rabbit():
     
 def requirements():
     with prefix('. /opt/ebs/venv/bin/activate'):
+        local("for line in `cat /opt/ebs/data/soft/del_requirements.txt`; do pip uninstall -y -q $line; done")
         local('pip install -U -r /opt/ebs/data/soft/requirements.txt')
     
 def virtualenv():
