06508b336245891fc2566632b4484743bba643b3
dolphinik <brat002@gmail.com>
2013-03-15 09:53:46 +0000

    + add w1ru

--- webadmin/ebscab/payments/ru_sberbank/urls.py
+++ webadmin/ebscab/payments/ru_sberbank/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from views import PayView
 
 urlpatterns = patterns('',
-    url(r'^/ru-sberbank/payment/$', PayView.as_view(), name='getpaid-rusberbank-pay'),
+    url(r'^ru-sberbank/payment/$', PayView.as_view(), name='getpaid-rusberbank-pay'),
 )
---
+++ webadmin/ebscab/payments/w1ru/views.py
@@ -0,0 +1,44 @@
+import logging
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.base import View
+from payments.w1ru import PaymentProcessor
+from getpaid.models import Payment
+from django.conf import settings
+from BeautifulSoup import BeautifulSoup
+
+from billservice.models import Account
+logger = logging.getLogger('payments.w1ru')
+from django.views.generic.base import TemplateView
+
+
+class PayView(View):
+    def post(self, request, *args, **kwargs):
+
+
+        try:
+                status = PaymentProcessor.postback(request)
+
+                
+        except KeyError:
+            logger.warning('Got malformed POST request: %s' % str(request.POST))
+            return HttpResponse('MALFORMED')
+
+        return HttpResponse(status)
+
+
+
+
+class SuccessView(TemplateView):
+
+    template_name = "account/payment_success.html"
+
+    def get_context_data(self, **kwargs):
+        return {}
+    
+class FailureView(TemplateView):
+
+    template_name = "account/payment_failure.html"
+
+    def get_context_data(self, **kwargs):
+        return {}
