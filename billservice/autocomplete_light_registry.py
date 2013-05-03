4d717ec834e72d67c4f40205894dbb0fd1fbe935
dolphinik <brat002@gmail.com>
2013-05-03 13:13:05 +0000

    * fixes

--- billservice/autocomplete_light_registry.py
+++ billservice/autocomplete_light_registry.py
@@ -8,13 +8,37 @@ class AccountAutocomplete(autocomplete_light.AutocompleteModelTemplate):
     search_fields = ['^username', 'contract', 'fullname']
     choice_template='template_autocomplete/templated_choice.html'
 
+    def choices_for_request(self):
+        choices = super(AccountAutocomplete, self).choices_for_request()
+
+        if not self.request.user.is_staff:
+            return  []
+
+        return choices
+    
 class UserAutocomplete(autocomplete_light.AutocompleteModelTemplate):
     search_fields = ['username']
     choice_template='autocomplete_light/model_template/nourl_choice.html'
     
+    def choices_for_request(self):
+        choices = super(UserAutocomplete, self).choices_for_request()
+
+        if not self.request.user.is_staff:
+            return  []
+
+        return choices
+    
 class SystemUserAutocomplete(autocomplete_light.AutocompleteModelTemplate):
     search_fields = ['username']
     choice_template='template_autocomplete/templated_choice.html'
+    
+    def choices_for_request(self):
+        choices = super(SystemUserAutocomplete, self).choices_for_request()
+
+        if not self.request.user.is_staff:
+            return  []
+
+        return choices
     
 autocomplete_light.register(Account, AccountAutocomplete,     autocomplete_js_attributes={
         'minimum_characters': 0,
