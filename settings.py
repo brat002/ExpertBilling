ab78f20044f288f3b68966d14c288a20fd160fc9
dolphinik <brat002@gmail.com>
2013-05-21 08:27:42 +0000

    + add migrations

--- settings.py
+++ settings.py
@@ -90,6 +90,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
     #'lib.threadlocals.ThreadLocalsMiddleware',
