762613e38d89e45eb797154c41b13585aed83207
dolphinik <brat002@gmail.com>
2013-07-01 13:20:37 +0000

    + added simpleterminal payment system

---
+++ webadmin/ebscab/payments/simpleterminal/views.py
@@ -0,0 +1,31 @@
+import logging
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.base import View
+from payments.simpleterminal import PaymentProcessor
+from getpaid.models import Payment
+from django.conf import settings
+
+from billservice.models import Account
+logger = logging.getLogger('payments.simpleterminal')
+
+class PayView(View):
+    def get(self, request, *args, **kwargs):
+
+        if PaymentProcessor.get_backend_setting('SECRET') not in ['', None]:
+            if request.GET.get('secret')!=PaymentProcessor.get_backend_setting('SECRET'):
+                return '-100'
+
+        try:
+
+            status = PaymentProcessor.pay(request, request.GET)
+                
+        except KeyError:
+            logger.warning('Got malformed POST request: %s' % str(request.GET))
+            return HttpResponse('MALFORMED')
+
+        return HttpResponse(status)
+
+
+
+
