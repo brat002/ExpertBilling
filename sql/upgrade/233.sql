2ae287f7d0f22b629630591301452f4cab187141
dolphinik <brat002@gmail.com>
2013-07-29 15:24:00 +0000

    ++ add platezhka terminal

---
+++ sql/upgrade/233.sql
@@ -0,0 +1,2 @@
+CREATE INDEX "billservice_account_deleted" ON "billservice_account" ("deleted");
+CREATE INDEX "billservice_periodicalservice_deleted" ON "billservice_periodicalservice" ("deleted");
