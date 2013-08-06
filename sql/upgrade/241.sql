646e9b6285b57ccfa6b7005058bcf1d32225aba4
dolphinik <brat002@gmail.com>
2013-08-06 21:01:39 +0000

    * bugfixes

--- sql/upgrade/241.sql
+++ sql/upgrade/241.sql
@@ -1,10 +1,24 @@
-CREATE TABLE "ebsadmin_comment" (
-    "id" serial NOT NULL PRIMARY KEY,
-    "content_type_id" integer REFERENCES "django_content_type" ("id") DEFERRABLE INITIALLY DEFERRED,
-    "object_id" integer CHECK ("object_id" >= 0),
-    "comment" text NOT NULL,
-    "notify_date" timestamp with time zone NOT NULL,
-    "due_date" timestamp with time zone NOT NULL,
-    "deleted" timestamp with time zone NOT NULL
+CREATE TABLE ebsadmin_comment
+(
+  id serial NOT NULL,
+  content_type_id integer,
+  object_id integer,
+  comment text NOT NULL,
+  created timestamp with time zone,
+  done_date timestamp with time zone,
+  done_systemuser_id integer,
+  due_date timestamp with time zone,
+  deleted timestamp with time zone,
+  done_comment text DEFAULT ''::text,
+  CONSTRAINT ebsadmin_comment_pkey PRIMARY KEY (id),
+  CONSTRAINT ebsadmin_comment_content_type_id_fkey FOREIGN KEY (content_type_id)
+      REFERENCES django_content_type (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED,
+  CONSTRAINT ebsadmin_comment_done_systemuser_id_fkey FOREIGN KEY (done_systemuser_id)
+      REFERENCES billservice_systemuser (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION DEFERRABLE INITIALLY DEFERRED,
+  CONSTRAINT ebsadmin_comment_object_id_check CHECK (object_id >= 0)
 )
-;
+WITH (
+  OIDS=FALSE
+);
