c68c0e460b8b1303ee79f577eeae55bc3edfe32c
dolphinik <brat002@gmail.com>
2013-09-14 22:13:11 +0000

    * fix

--- classes/core_class/RadiusSession.py
+++ classes/core_class/RadiusSession.py
@@ -5,32 +5,32 @@ class RadiusSession(tuple):
 
     __slots__ = () 
 
-    _fields = ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address', 'speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id', 'guest_pool', 'nas_port_id') 
+    _fields = ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address', 'speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id', 'guest_pool', 'nas_port_id', 'speed_change_queued', 'pod_queued')
     def __getstate__(self):
         return tuple(self)
     def __setstate__(self, state):
         return self._make(state)
-    def __new__(cls, id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id,guest_pool, nas_port_id):
-        return tuple.__new__(cls, (id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id,guest_pool, nas_port_id)) 
+    def __new__(cls, id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id,guest_pool, nas_port_id, speed_change_queued, pod_queued):
+        return tuple.__new__(cls, (id, account_id, subaccount_id, sessionid, framed_ip_address, speed_string, access_type, nas_id, time_from_last_update, date_start,ipinuse_id,caller_id,guest_pool, nas_port_id, speed_change_queued, pod_queued))
 
     @classmethod
     def _make(cls, iterable, new=tuple.__new__, len=len):
         'Make a new RadiusSession object from a sequence or iterable'
         result = new(cls, iterable)
         if len(result) != 14:
-            raise TypeError('Expected 14 arguments, got %d' % len(result))
+            raise TypeError('Expected 16 arguments, got %d' % len(result))
         return result 
 
     def __repr__(self):
-        return 'RadiusSession(id=%r, account_id=%r, subaccount_id=%r, sessionid=%r, framed_ip_address=%r, speed_string=%r, access_type=%r, nas_id=%r, time_from_last_update=%r, date_start=%r, ipinuse_id=%r, caller_id=%r, guest_pool=%r, nas_port_id=%r)' % self 
+        return 'RadiusSession(id=%r, account_id=%r, subaccount_id=%r, sessionid=%r, framed_ip_address=%r, speed_string=%r, access_type=%r, nas_id=%r, time_from_last_update=%r, date_start=%r, ipinuse_id=%r, caller_id=%r, guest_pool=%r, nas_port_id=%r, speed_change_queued=%r, pod_queued=%r)' % self
 
     def _asdict(t):
         'Return a new dict which maps field names to their values'
-        return {'id': t[0], 'account_id': t[1], 'subaccount_id':t[2], 'sessionid': t[3], 'framed_ip_address':t[4], 'speed_string': t[5], 'access_type': t[6], 'nas_id': t[7], 'time_from_last_update':t[8], 'date_start':t[9], 'ipinuse_id':t[10], 'caller_id':t[11], 'guest_pool':t[12], 'nas_port_id':t[13]} 
+        return {'id': t[0], 'account_id': t[1], 'subaccount_id':t[2], 'sessionid': t[3], 'framed_ip_address':t[4], 'speed_string': t[5], 'access_type': t[6], 'nas_id': t[7], 'time_from_last_update':t[8], 'date_start':t[9], 'ipinuse_id':t[10], 'caller_id':t[11], 'guest_pool':t[12], 'nas_port_id':t[13], 'speed_change_queued': t[14], 'pod_queued': t[15]}
 
     def _replace(self, **kwds):
         'Return a new RadiusSession object replacing specified fields with new values'
-        result = self._make(map(kwds.pop, ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address','speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id', 'guest_pool', 'nas_port_id'), self))
+        result = self._make(map(kwds.pop, ('id', 'account_id', 'subaccount_id','sessionid', 'framed_ip_address','speed_string', 'access_type', 'nas_id', 'time_from_last_update', 'date_start', 'ipinuse_id', 'caller_id', 'guest_pool', 'nas_port_id', 'speed_change_queued', 'pod_queued'), self))
         if kwds:
             raise ValueError('Got unexpected field names: %r' % kwds.keys())
         return result 
@@ -52,5 +52,7 @@ class RadiusSession(tuple):
     caller_id = property(itemgetter(11))
     guest_pool = property(itemgetter(12))
     nas_port_id = property(itemgetter(13))
+    speed_change_queued = property(itemgetter(14))
+    pod_queued = property(itemgetter(15))
     
     
