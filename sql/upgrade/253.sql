32e5511c0165dd1bb62f521f6011408d185d42e5
dolphinik <brat002@gmail.com>
2013-09-23 19:24:57 +0000

    * fix and add new report

---
+++ sql/upgrade/253.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION get_free_ip_from_pool(ag_pool_id_ integer)
+  RETURNS inet AS
+$BODY$
+BEGIN
+
+RETURN (SELECT free_ip.ipaddress FROM 
+(SELECT 
+(SELECT start_ip FROM billservice_ippool WHERE id=ag_pool_id_) + ip_series.ip_inc 
+FROM generate_series(0, (SELECT end_ip - start_ip FROM billservice_ippool WHERE id=ag_pool_id_))AS ip_series(ip_inc)) AS free_ip(ipaddress)
+WHERE free_ip.ipaddress NOT IN (SELECT ip FROM billservice_ipinuse WHERE pool_id=ag_pool_id_ and ((ack=True and disabled is Null) or 
+(disabled is NULL and ack=False and datetime>now()-interval '5 seconds' ) or (disabled is not null and disabled>now()-interval '5 minutes'))) ORDER BY RANDOM() LIMIT 1);
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+  
+CREATE OR REPLACE FUNCTION get_free_ip_from_pool(ag_pool_id_ integer, acct_interval integer)
+  RETURNS inet AS
+$BODY$
+BEGIN
+
+RETURN (SELECT free_ip.ipaddress FROM 
+(SELECT 
+(SELECT start_ip FROM billservice_ippool WHERE id=ag_pool_id_) + ip_series.ip_inc 
+FROM generate_series(0, (SELECT end_ip - start_ip FROM billservice_ippool WHERE id=ag_pool_id_))AS ip_series(ip_inc)) AS free_ip(ipaddress)
+WHERE free_ip.ipaddress NOT IN (SELECT ip FROM billservice_ipinuse WHERE pool_id=ag_pool_id_ and ((ack=True and disabled is Null) or 
+(disabled is NULL and ack=False and datetime>now()- interval '5 seconds') or (disabled is not null and disabled>now()-(acct_interval*3 || ' seconds')::INTERVAL) or (lost is not null and lost>now()-interval '30 minutes'))) ORDER BY RANDOM() LIMIT 1);
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
