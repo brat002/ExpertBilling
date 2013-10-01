59478fec689b5269ff5bfd86e0e7149c5d0406b6
dolphinik <brat002@gmail.com>
2013-10-01 08:28:39 +0000

    * fix

--- cacherouter.py
+++ cacherouter.py
@@ -406,7 +406,7 @@ class Cache(object):
         if obj: return obj
         
         
-        self.cursor.execute("""SELECT accs.id, accs.service_id, accs.account_id, date_trunc('second',accs.activated) as activated, date_trunc('second',accs.deactivated) as deactivated, accs.action_status, 
+        self.cursor.execute("""SELECT accs.id, accs.service_id, accs.account_id, accs.activated, accs.deactivated, accs.action_status, 
     
                                     accs.speed_status, accs.temporary_blocked, date_trunc('second',accs.last_checkout) as last_checkout, accs.subaccount_id, COALESCE(accs.cost, addons.cost) as cost
     
@@ -427,7 +427,7 @@ class Cache(object):
         if obj: return obj
         
         
-        self.cursor.execute("""SELECT accs.id, accs.service_id, accs.account_id, date_trunc('second',accs.activated) as activated, date_trunc('second',accs.deactivated) as deactivated, accs.action_status, 
+        self.cursor.execute("""SELECT accs.id, accs.service_id, accs.account_id, accs.activated, accs.deactivated, accs.action_status, 
     
                                     accs.speed_status, accs.temporary_blocked, date_trunc('second',accs.last_checkout) as last_checkout, accs.subaccount_id, COALESCE(accs.cost, addons.cost) as cost
     
--- core.py
+++ core.py
@@ -251,18 +251,20 @@ class check_vpn_access(Thread):
                 #cur.execute("UPDATE billservice_ipinuse SET disabled=now() WHERE dynamic=True and disabled is Null and ip::inet not in (SELECT DISTINCT framed_ip_address::inet FROM radius_activesession WHERE ipinuse_id is not NUll and (session_status='ACTIVE'));")    
                 #cur.connection.commit()   
                 
-                for key, value in dublicated_ips.iteritems():
-                    if len(value)<=1: continue
-                    logger.debug("%s: Dublicated IP detected %s %s", (self.getName(), key, value))
-                    value = sorted(value, key=attrgetter('date_start'))
-                    first = True
-                    for rs in value:
-                        if first == True:
-                            first = False
-                            continue
-                        logger.debug("%s: Send dublicates remove POD: account:  %s| nas: %s | sessionid: %s", (self.getName(), acc.account_id, nas.id, str(rs.sessionid)))
-                        PoD.delay(acc._asdict(), subacc._asdict(), nas._asdict(), access_type=rs.access_type, session_id=str(rs.sessionid), vpn_ip_address=rs.framed_ip_address, caller_id=str(rs.caller_id), format_string=str(nas.reset_action))
-                        logger.debug("%s: dublicates remove POD sended: account:  %s| nas: %s | sessionid: %s", (self.getName(), acc.account_id, nas.id, str(rs.sessionid)))
+                #===============================================================
+                # for key, value in dublicated_ips.iteritems():
+                #    if len(value)<=1: continue
+                #    logger.debug("%s: Dublicated IP detected %s %s", (self.getName(), key, value))
+                #    value = sorted(value, key=attrgetter('date_start'))
+                #    first = True
+                #    for rs in value:
+                #        if first == True:
+                #            first = False
+                #            continue
+                #        logger.debug("%s: Send dublicates remove POD: account:  %s| nas: %s | sessionid: %s", (self.getName(), acc.account_id, nas.id, str(rs.sessionid)))
+                #        PoD.delay(acc._asdict(), subacc._asdict(), nas._asdict(), access_type=rs.access_type, session_id=str(rs.sessionid), vpn_ip_address=rs.framed_ip_address, caller_id=str(rs.caller_id), format_string=str(nas.reset_action))
+                #        logger.debug("%s: dublicates remove POD sended: account:  %s| nas: %s | sessionid: %s", (self.getName(), acc.account_id, nas.id, str(rs.sessionid)))
+                #===============================================================
 
                 cur.close()
                 logger.info("VPNALIVE: VPN thread run time: %s", time.time() - a)
