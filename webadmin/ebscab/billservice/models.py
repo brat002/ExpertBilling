180e48ea647b2edb94470106a277adffcb2a7870
brat002 <brat002@gmail.com>
2013-10-14 01:42:29 +0300

    + add registrationrequest

--- webadmin/ebscab/billservice/models.py
+++ webadmin/ebscab/billservice/models.py
@@ -2599,3 +2599,24 @@ class SuppAgreement(models.Model):
         #    return "%s?id=%s" % (reverse('permissiongroup_delete'), self.id)
         #===========================================================================
     
+    
+class RegistrationRequest(models.Model):
+    
+    fullname = models.CharField(verbose_name = _(u'ФИО'), max_length=512)
+    address = models.CharField(verbose_name = _(u'Адрес'), max_length=1024)
+    phone = models.CharField(verbose_name = _(u'Телефон'), max_length=64)
+    email = models.EmailField(verbose_name = _(u'Email'), unique = True)
+    datetime = models.DateTimeField(verbose_name = _(u'Дата'), auto_now_add=True)
+    
+    
+    def __unicode__(self):
+        return u"%s" % self.name
+
+    class Meta:
+        ordering = ['datetime', ]
+        verbose_name = _(u"Заявка на подключение")
+        verbose_name_plural = _(u"Заявки на подключение")
+        
+    def get_remove_url(self):
+        return "%s?id=%s" % (reverse('registrationrequest_delete'), self.id)
+    
