d3b90fa818a4ff3ab0500609ed65b14eeb84940b
brat002 <brat002@gmail.com>
2013-10-13 12:21:33 +0300

    * fixes in charts

--- build.py
+++ build.py
@@ -13,6 +13,7 @@ def build(key=None, users=200):
     with cd('mikrobill'):
         run('git pull')
         run('sh build_all.sh %s %s %s' % (key if key else 'demo1.5_`uname -i`', users, key if key else ''))
+        run('scp builds/%s.tar.gz brat002@m3.diggit.ru:/opt/ebs/media/builds/' % (key if key else 'demo1.5_`uname -i`'))
        
 @hosts(['brat002@m4.diggit.ru'])
 def update_demo():
