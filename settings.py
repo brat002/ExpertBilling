9a201d287611cb098cbc2141807d0178caecb7a9
brat002 <brat002@gmail.com>
2013-10-15 00:31:24 +0300

    + add news to main page + add logo

--- settings.py
+++ settings.py
@@ -79,6 +79,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.contrib.messages.context_processors.messages',
     'lib.context_processors.default_current_view_name',
+    'lib.context_processors.project_settings',
     
 )
 
