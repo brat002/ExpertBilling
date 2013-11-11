8e5b53b214f1fba9e5024d6f924bb4dd5db0e8b4
Alexandr Kuzmitsky <a_kuzmitsky@wargaming.net>
2013-11-12 01:52:00 +0300

    Remove split code

--- webadmin/ebscab/chartit/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This Django application can be used to create charts and pivot charts 
-directly from models.
-"""
-from .chartdata import PivotDataPool, DataPool
-from .charts import PivotChart, Chart
--- webadmin/ebscab/payments/liqpay/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from views import PayView
-
-urlpatterns = patterns('',
-                       url(r'^liqpay/payment/result/$', PayView.as_view(), name='getpaid-liqpay-pay'),
-                       url(r'^liqpay/payment/$', PayView.as_view(), name='payment-result'),
-    
-)
