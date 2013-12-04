1f48670686365480ee5e14d92a8de4a8e1514d99
Igor Petrov <igor.s.petrov@gmail.com>
2013-12-04 18:16:57 +0400

    Изменения в accounts_edit.js

--- static/js/accounts_edit.js
+++ static/js/accounts_edit.js
@@ -22,4 +22,19 @@ $(document).ready(function() {
     event.preventDefault(event);
   });
 
+  // отображение полей для юр. лица при выборе соответствующего признака
+  $('#id_organization').click(function() {
+    if ($('#id_organization').is(':checked')){
+      $('#organization-fieldset').show();
+    } else {
+      $('#organization-fieldset').hide();
+    }
+  });
+
+  if ($('#id_organization').is(':checked')){
+    $('#organization-fieldset').show();
+  }else{
+    $('#organization-fieldset').hide();
+  }
+
 });
