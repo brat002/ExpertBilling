02b8e617111bdac66092713ccf5abddd9fd8d8e0
Igor Petrov <igor.s.petrov@gmail.com>
2013-12-04 17:03:15 +0400

    fix

--- ebsadmin/urls.py
+++ ebsadmin/urls.py
@@ -608,6 +608,6 @@ urlpatterns += patterns('ebsadmin.views',
      url(r'^test_credentials/$', 'testCredentials'),
 )
 
-urlpatterns = patterns('',
+urlpatterns += patterns('',
     (r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
 )
