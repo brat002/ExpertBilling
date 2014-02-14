50f6bb5a3713b90b2fc10488e12acc9ffae2ffb6
Alexandr Kuzmitsky <a_kuzmitsky@wargaming.net>
2014-02-15 00:27:21 +0300

    Add sms template

---
+++ sql/upgrade/265.sql
@@ -0,0 +1,8 @@
+INSERT INTO billservice_documenttype(
+            id, name)
+    VALUES (9, 'Шаблон SMS сообщения');
+INSERT INTO billservice_templatetype(
+            id, name)
+    VALUES (11, 'Шаблон SMS сообщения');
+    
+    
