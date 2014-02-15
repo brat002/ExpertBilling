f9a8c0160097275e0f3c5a15265bd4c3108926e4
Alexandr Kuzmitsky <a_kuzmitsky@wargaming.net>
2014-02-15 09:16:45 +0300

    Add notifications and new pull rquest

--- billservice/forms.py
+++ billservice/forms.py
@@ -1511,9 +1511,12 @@ class SendSmsForm(forms.Form):
     bc = get_backend_choices()
     accounts = forms.ModelMultipleChoiceField(queryset=Account.objects.filter(phone_m__isnull=False), widget = forms.widgets.MultipleHiddenInput)
     backend = forms.ChoiceField(label=_(u'Оператор'), choices = bc, initial = bc[0][0] if bc else '', widget = forms.widgets.Select(attrs={'rows':4, 'class': 'input-large span5'}))
-    body = forms.CharField(label=_(u'Сообщение'), widget = forms.widgets.Textarea(attrs={'rows':4, 'class': 'input-large span5'}), help_text=_(u"Можно использовать {{account.ballance}}, {{account.fullname}}, {{account.username}}, {{account.contract}}"))
+    template = forms.ChoiceField(label=_(u'Шаблон'), choices = [], required=False, widget = forms.widgets.Select(attrs={'rows':4, 'class': 'input-large span5'}))
+    body = forms.CharField(label=_(u'Сообщение'), widget = forms.widgets.Textarea(attrs={'id': 'id_sendsms_body', 'rows':4, 'class': 'input-large span5'}), help_text=_(u"Можно использовать {{account.ballance}}, {{account.fullname}}, {{account.username}}, {{account.contract}}"))
     publish_date = forms.DateTimeField(label=_(u'Опубликовать'), help_text = _(u'Не указывайте, если сообщения должны быть отправлены сразу'), required = False, widget=forms.widgets.DateTimeInput(attrs={'class':'datepicker'}))
-    
+    def __init__(self, *args, **kwargs):
+        super(SendSmsForm, self).__init__(*args, **kwargs)
+        self.fields['template'].choices=[['', '---']]+list(Template.objects.filter(type__id=11).values_list('body', 'name', ))
 
     
 class DynamicSchemaFieldForm(forms.ModelForm):
