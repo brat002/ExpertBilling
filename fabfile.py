127804b2897ac7186ed631e87ab0b0c91c6d8663
Alexandr Kuzmitsky <a_kuzmitsky@wargaming.net>
2014-11-13 18:11:19 +0300

    Add try except for pip

--- fabfile.py
+++ fabfile.py
@@ -36,8 +36,9 @@ def prepare_deploy():
     #local('apt-get -y install g++ postgresql-9.1 postgresql-contrib-9.1 postgresql-server-dev-9.1 htop mc python-dev mc openssh-server openssl python-paramiko python-crypto libapache2-mod-wsgi python-simplejson rrdtool snmp python-pexpect python-pip python-virtualenv rabbitmq-server ')
     local('apt-get  -y --force-yes  install libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev g++ postgresql postgresql-contrib postgresql-server-dev-all htop mc python-dev mc openssh-server openssl python-paramiko python-crypto libapache2-mod-wsgi python-simplejson rrdtool snmp python-pexpect python-pip python-virtualenv redis-server rabbitmq-server libmemcached-dev memcached libjpeg-dev libfreetype6-dev zlib1g-dev')
     local('pip install psycopg2')
-    local('pip install datetime')
-    local('pip install dateutils')
+    with settings(warn_only=True):
+        local('pip install datetime')
+        local('pip install dateutils')
 
 def configure_rabbit():
     with settings(warn_only=True):
