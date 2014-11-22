1517bc7887858b9c694d8c324b04ebae1cf3bc01
Alexandr Kuzmitsky <a_kuzmitsky@wargaming.net>
2014-11-22 13:32:40 +0300

    Experimental support for deltas in periodical service

--- classes/core_class/PeriodicalServiceSettlementData.py
+++ classes/core_class/PeriodicalServiceSettlementData.py
@@ -1,36 +1,36 @@
 from operator import itemgetter, setitem
 
 class PeriodicalServiceSettlementData(tuple):
-    'PeriodicalServiceSettlementData(ps_id, ps_name, cost, cash_method, sp_name, time_start, length, length_in, autostart, tarif_id, condition, condition_summ, created, deactivated, deleted, tpd)' 
+    'PeriodicalServiceSettlementData(ps_id, ps_name, cost, cash_method, sp_name, time_start, length, length_in, autostart, tarif_id, condition, condition_summ, created, deactivated, deleted, tpd, delta_from_ballance)' 
 
     __slots__ = () 
 
-    _fields = ('ps_id', 'ps_name', 'cost', 'cash_method', 'sp_name', 'time_start', 'length', 'length_in', 'autostart', 'tarif_id', 'condition', 'condition_summ', 'created', 'deactivated', 'deleted', 'tpd') 
+    _fields = ('ps_id', 'ps_name', 'cost', 'cash_method', 'sp_name', 'time_start', 'length', 'length_in', 'autostart', 'tarif_id', 'condition', 'condition_summ', 'created', 'deactivated', 'deleted', 'tpd', 'delta_from_ballance') 
     def __getstate__(self):
         return tuple(self)
     def __setstate__(self, state):
         return self._make(state)
-    def __new__(cls, ps_id, ps_name, cost, cash_method, sp_name, time_start, length, length_in, autostart, tarif_id, condition, condition_summ, created, deactivated, deleted, tpd):
-        return tuple.__new__(cls, (ps_id, ps_name, cost, cash_method, sp_name, time_start, length, length_in, autostart, tarif_id, condition, condition_summ, created, deactivated, deleted, tpd)) 
+    def __new__(cls, ps_id, ps_name, cost, cash_method, sp_name, time_start, length, length_in, autostart, tarif_id, condition, condition_summ, created, deactivated, deleted, tpd, delta_from_ballance):
+        return tuple.__new__(cls, (ps_id, ps_name, cost, cash_method, sp_name, time_start, length, length_in, autostart, tarif_id, condition, condition_summ, created, deactivated, deleted, tpd, delta_from_ballance)) 
 
     @classmethod
     def _make(cls, iterable, new=tuple.__new__, len=len):
         'Make a new PeriodicalServiceSettlementData object from a sequence or iterable'
         result = new(cls, iterable)
-        if len(result) != 16:
-            raise TypeError('Expected 16 arguments, got %d' % len(result))
+        if len(result) != 17:
+            raise TypeError('Expected 17 arguments, got %d' % len(result))
         return result 
 
     def __repr__(self):
-        return 'PeriodicalServiceSettlementData(ps_id=%r, ps_name=%r, cost=%r, cash_method=%r, sp_name=%r, time_start=%r, length=%r, length_in=%r, autostart=%r, tarif_id=%r, condition=%r, condition_summ=%s, created=%r, deactivated=%r, deleted=%r, tpd=%r)' % self 
+        return 'PeriodicalServiceSettlementData(ps_id=%r, ps_name=%r, cost=%r, cash_method=%r, sp_name=%r, time_start=%r, length=%r, length_in=%r, autostart=%r, tarif_id=%r, condition=%r, condition_summ=%s, created=%r, deactivated=%r, deleted=%r, tpd=%r, delta_from_ballance=%r)' % self 
 
     def _asdict(t):
         'Return a new dict which maps field names to their values'
-        return {'ps_id': t[0], 'ps_name': t[1], 'cost': t[2], 'cash_method': t[3], 'sp_name': t[4], 'time_start': t[5], 'length': t[6], 'length_in': t[7], 'autostart': t[8], 'tarif_id': t[9], 'condition': t[10], 'condition_summ': t[11],  'created': t[12], 'deactivated':t[13], 'deleted':t[14], 'tpd': t[15]} 
+        return {'ps_id': t[0], 'ps_name': t[1], 'cost': t[2], 'cash_method': t[3], 'sp_name': t[4], 'time_start': t[5], 'length': t[6], 'length_in': t[7], 'autostart': t[8], 'tarif_id': t[9], 'condition': t[10], 'condition_summ': t[11],  'created': t[12], 'deactivated':t[13], 'deleted':t[14], 'tpd': t[15], 'delta_from_ballance': t[16]} 
 
     def _replace(self, **kwds):
         'Return a new PeriodicalServiceSettlementData object replacing specified fields with new values'
-        result = self._make(map(kwds.pop, ('ps_id', 'ps_name', 'cost', 'cash_method', 'sp_name', 'time_start', 'length', 'length_in', 'autostart', 'tarif_id', 'condition', 'condition_summ', 'created', 'deactivated', 'deleted', 'tpd'), self))
+        result = self._make(map(kwds.pop, ('ps_id', 'ps_name', 'cost', 'cash_method', 'sp_name', 'time_start', 'length', 'length_in', 'autostart', 'tarif_id', 'condition', 'condition_summ', 'created', 'deactivated', 'deleted', 'tpd', 'delta_from_ballance'), self))
         if kwds:
             raise ValueError('Got unexpected field names: %r' % kwds.keys())
         return result 
@@ -54,3 +54,4 @@ class PeriodicalServiceSettlementData(tuple):
     deactivated = property(itemgetter(13))
     deleted = property(itemgetter(14))
     tpd = property(itemgetter(15))
+    delta_from_ballance = property(itemgetter(16))
