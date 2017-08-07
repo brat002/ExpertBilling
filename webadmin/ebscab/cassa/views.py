be81972c3dc854271c5a58894b5c90966b82e45a
Sergey Pankov <pankov.seryi@gmail.com>
2017-08-07 23:58:27 +0300

    Update app cassa
    - change codestyle follow PEP8
    - remove unused imports
    - sort imports
    - remove unused code

--- webadmin/ebscab/cassa/views.py
+++ webadmin/ebscab/cassa/views.py
@@ -1,46 +1,69 @@
-#-*-coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from billservice.models import (
+    Account,
+    SystemUser,
+    Transaction,
+    TransactionType
+)
+from lib.decorators import render_to
 
 from cassa.forms import PayForm
-from lib.decorators import render_to
-from billservice.models import SystemUser, Transaction, Account, TransactionType
-import datetime
+
+
 @render_to("cassa/index.html")
 def index(request):
-    message=''
+    message = ''
     if request.method == 'POST':
         form = PayForm(request.POST)
         if form.is_valid():
-            username=form.cleaned_data['username']
-            password=form.cleaned_data['password']
-            summ=form.cleaned_data['summ']
-            account_username=form.cleaned_data['account_username']
-            description=form.cleaned_data['description']
-            promise=form.cleaned_data['promise']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            summ = form.cleaned_data['summ']
+            account_username = form.cleaned_data['account_username']
+            description = form.cleaned_data['description']
+            promise = form.cleaned_data['promise']
             try:
-                user=SystemUser.objects.get(username=username, text_password=password)
+                user = SystemUser.objects.get(
+                    username=username, text_password=password)
             except:
-                return {'form':form,'message':u'Пользователя с таким логином и паролем не существует',}
+                return {
+                    'form': form,
+                    'message': (u'Пользователя с таким логином и паролем '
+                                u'не существует')
+                }
 
             try:
-                account=Account.objects.get(username=account_username)
+                account = Account.objects.get(username=account_username)
             except:
-                return {'form':form,'message':u'Пользователя с таким логином и паролем не существует',}
-            
-            t=Transaction()
-            t.account=account
-            t.type=TransactionType.objects.get(internal_name='MANUAL_TRANSACTION')
-            t.approved=True
-            t.summ=-1*summ
-            t.description=description
-            t.created=datetime.datetime.now()
-            t.promise=promise
-            t.systemuser=user
+                return {
+                    'form': form,
+                    'message': (u'Пользователя с таким логином и паролем '
+                                u'не существует')
+                }
+
+            t = Transaction()
+            t.account = account
+            t.type = TransactionType.objects.get(
+                internal_name='MANUAL_TRANSACTION')
+            t.approved = True
+            t.summ = -1 * summ
+            t.description = description
+            t.created = datetime.datetime.now()
+            t.promise = promise
+            t.systemuser = user
             t.save()
-            account=Account.objects.get(username=account_username)
-            
-            message=u'Платёж успешно выполнен. Новый баланс пользователя %s' % account.ballance
-            
+            account = Account.objects.get(username=account_username)
+
+            message = (u'Платёж успешно выполнен. '
+                       u'Новый баланс пользователя %s') % account.ballance
+
     else:
         form = PayForm()
-        
-    return {'form':form,'message':message}
+
+    return {
+        'form': form,
+        'message': message
+    }
