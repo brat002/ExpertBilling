24a3a7d0e7241227b57acae66d0b4a525807b914
Sergey Pankov <pankov.seryi@gmail.com>
2017-08-09 23:40:51 +0300

    Update app /webadmin/ebscab/radius
    - change codestyle follow PEP8
    - remove unused imports
    - sort imports
    - remove unused code
    - add to file top: # -*- coding: utf-8 -*-

--- webadmin/ebscab/radius/forms.py
+++ webadmin/ebscab/radius/forms.py
@@ -1,19 +1,41 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
 from django import forms
-from nas.models import Nas
-from ajax_select.fields import AutoCompleteSelectMultipleField
-from billservice.forms import SplitDateTimeWidget
-from billservice.models import City
 from django.utils.translation import ugettext_lazy as _
+
+from ajax_select.fields import AutoCompleteSelectMultipleField
+from billservice.models import City
+from nas.models import Nas
 
 
 class SessionFilterForm(forms.Form):
-    account = AutoCompleteSelectMultipleField( 'account_fts', required = False) 
-    nas = forms.ModelMultipleChoiceField(label=_(u"Сервер доступа субаккаунта"), queryset=Nas.objects.all(), required=False)
+    account = AutoCompleteSelectMultipleField('account_fts', required=False)
+    nas = forms.ModelMultipleChoiceField(
+        label=_(u"Сервер доступа субаккаунта"),
+        queryset=Nas.objects.all(),
+        required=False
+    )
     id = forms.IntegerField(required=False)
-    city = forms.ModelChoiceField(label=_(u'Город'), queryset=City.objects.all(), required=False)
+    city = forms.ModelChoiceField(
+        label=_(u'Город'), queryset=City.objects.all(), required=False)
     street = forms.CharField(label=_(u'Улица'), required=False)
     house = forms.CharField(label=_(u'Дом'), required=False)
-    date_start = forms.DateTimeField(label = _(u'С даты'), required=False, widget = forms.widgets.DateTimeInput(attrs={'class':'datepicker'}))
-    date_end = forms.DateTimeField(label = _(u'По дату'), required=False, widget = forms.widgets.DateTimeInput(attrs={'class':'datepicker'}))
-    only_active = forms.BooleanField(label = _(u'Только активные'), widget = forms.widgets.CheckboxInput, required=False)
+    date_start = forms.DateTimeField(
+        label=_(u'С даты'),
+        required=False,
+        widget=forms.widgets.DateTimeInput(attrs={
+            'class': 'datepicker'
+        })
+    )
+    date_end = forms.DateTimeField(
+        label=_(u'По дату'),
+        required=False,
+        widget=forms.widgets.DateTimeInput(attrs={
+            'class': 'datepicker'
+        })
+    )
+    only_active = forms.BooleanField(
+        label=_(u'Только активные'),
+        widget=forms.widgets.CheckboxInput,
+        required=False
+    )
