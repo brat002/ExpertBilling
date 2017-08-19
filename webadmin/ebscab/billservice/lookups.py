9ae614f0546afe50252a9440abc9ae5d4fcb7071
Sergey Pankov <pankov.seryi@gmail.com>
2017-08-19 15:27:04 +0300

    Move module content 'ebscab.lookups' to 'ebscab.billservice.lookups'

--- webadmin/ebscab/billservice/lookups.py
+++ webadmin/ebscab/billservice/lookups.py
@@ -4,9 +4,14 @@ from django.db.models import Q
 from django.utils.html import escape
 
 from ajax_select import LookupChannel
+from selectable.base import ModelLookup
+from selectable.decorators import login_required
+from selectable.registry import registry
+
 from billservice.models import (
     Account,
     City,
+    Hardware,
     House,
     Organization,
     Street,
@@ -283,3 +288,28 @@ class SubAccountFTSLookup(LookupChannel):
                                             escape(obj.vpn_ip_address),
                                             escape(obj.ipn_ip_address),
                                             escape(obj.ipn_mac_address))
+
+
+@login_required
+class HardwareLookup(ModelLookup):
+    model = Hardware
+    search_fields = (
+        'name__startswith',
+        'model__startswith',
+        'sn__startswith',
+        'comment__icontains'
+    )
+
+    def get_query(self, request, term):
+        return Hardware.objects.filter(accounthardware__isnull=True)
+
+    def get_item_label(self, item):
+        return u'%s %s sn:%s %s' % (
+            item.name,
+            item.model,
+            item.sn,
+            ('comment: %s' % item.comment) if item.comment else ''
+        )
+
+
+registry.register(HardwareLookup)
