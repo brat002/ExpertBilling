3818aa0dc0ebfd1bf9fd46402348ec9b34ca97d6
Sergey Pankov <pankov.seryi@gmail.com>
2017-08-19 17:49:37 +0300

    New-style 'wsgi.py'

--- webadmin/ebscab/ebscab/wsgi.py
+++ webadmin/ebscab/ebscab/wsgi.py
@@ -1,17 +1,27 @@
+"""
+WSGI config for inline_radio_select project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
 import os
+import site
 import sys
 
+from django.core.wsgi import get_wsgi_application
+
 sys.stdout = sys.stderr
+
+sys.path.append('/opt/ebs/web/')
+sys.path.append('/opt/ebs/web/ebscab/')
 sys.path.append('/opt/ebs/data/workers/')
-import site
+
 site.addsitedir('/opt/ebs/venv/lib/python2.6/site-packages')
 site.addsitedir('/opt/ebs/venv/lib/python2.7/site-packages')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inline_radio_select.settings")
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ebscab.settings'
-#path = '/opt/local/django'
-#if path not in sys.path:
-sys.path.append('/opt/ebs/web/')
-sys.path.append('/opt/ebs/web/ebscab/')
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
