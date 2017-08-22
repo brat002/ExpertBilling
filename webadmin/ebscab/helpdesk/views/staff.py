b9e66cfb235e114a090519a61e9b0036edd7d120
Sergey Pankov <pankov.seryi@gmail.com>
2017-08-22 12:06:42 +0300

    Replace 'request.user' to 'request.user.account' into 'filter(assigned_to=...)'
    See https://docs.djangoproject.com/en/1.11/releases/1.8/#query-relation-lookups-now-check-object-types

--- webadmin/ebscab/helpdesk/views/staff.py
+++ webadmin/ebscab/helpdesk/views/staff.py
@@ -87,7 +87,7 @@ def dashboard(request):
     """
 
     tickets = (Ticket.objects
-               .filter(assigned_to=request.user)
+               .filter(assigned_to=request.user.account)
                .exclude(status=Ticket.CLOSED_STATUS))
 
     ticket_table = UnpagedTicketTable(tickets)
@@ -726,7 +726,7 @@ def mass_update(request):
                 messages_sent_to.append(t.assigned_to.email)
 
             if t.queue.updated_ticket_cc and \
-                t.queue.updated_ticket_cc not in messages_sent_to:
+                    t.queue.updated_ticket_cc not in messages_sent_to:
                 send_templated_mail(
                     'closed_cc',
                     context,
